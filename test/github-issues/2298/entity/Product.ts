a91361a2eb8362d5f4cc699c36cda859b848073f
Umed Khudoiberdiev <zarrhost@gmail.com>
2018-06-08 00:09:33 +0500

    added test for #2298

---
+++ test/github-issues/2298/entity/Product.ts
@@ -0,0 +1,13 @@
+import {Entity, OneToMany, PrimaryGeneratedColumn} from "../../../../src";
+import {TicketProduct} from "./TicketProduct";
+
+@Entity()
+export class Product {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @OneToMany(type => TicketProduct, ticketp => ticketp.product)
+    ticketProduct: TicketProduct[];
+
+}
