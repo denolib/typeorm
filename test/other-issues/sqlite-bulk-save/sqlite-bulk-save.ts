fcee2179af7e306f18a7dd690ed8bac4ba7f07f1
Umed Khudoiberdiev <zarrhost@gmail.com>
2018-08-14 20:45:28 +0500

    Update sqlite-bulk-save.ts

--- test/other-issues/sqlite-bulk-save/sqlite-bulk-save.ts
+++ test/other-issues/sqlite-bulk-save/sqlite-bulk-save.ts
@@ -3,7 +3,7 @@ import {closeTestingConnections, createTestingConnections, reloadTestingDatabase
 import {Connection} from "../../../src/connection/Connection";
 import {Post} from "./entity/Post";
 
-describe.only("other issues > bulk save in sqlite", () => {
+describe("other issues > bulk save in sqlite", () => {
 
     let connections: Connection[];
     before(async () => connections = await createTestingConnections({
