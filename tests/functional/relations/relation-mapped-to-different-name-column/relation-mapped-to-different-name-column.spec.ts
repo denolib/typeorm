553c641621aea9abef4dc436c43458b13461658f
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-14 17:49:25 +0500

    working (relations - relation-mapped-to-different-name-column)

--- tests/functional/relations/relation-mapped-to-different-name-column/relation-mapped-to-different-name-column.spec.ts
+++ tests/functional/relations/relation-mapped-to-different-name-column/relation-mapped-to-different-name-column.spec.ts
@@ -1,19 +1,19 @@
 import "reflect-metadata";
 import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../../test/utils/test-utils";
-import {Connection} from "../../../../src/connection/Connection";
+import {Connection} from "../../../../src";
 import {Post} from "./entity/Post";
 import {PostDetails} from "./entity/PostDetails";
 
 describe.skip("relations > relation mapped to relation with different name (#56)", () => { // skipped because of CI error. todo: needs investigation
 
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
+    beforeAll(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
     }));
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("should work perfectly", () => Promise.all(connections.map(async connection => {
+    test("should work perfectly", () => Promise.all(connections.map(async connection => {
 
         // first create and save details
         const details = new PostDetails();
@@ -36,7 +36,7 @@ describe.skip("relations > relation mapped to relation with different name (#56)
             }
         });
 
-        posts.should.be.eql([{
+        expect(posts).toEqual([{
             id: 1,
             title: "Hello Post #1",
             details: {
