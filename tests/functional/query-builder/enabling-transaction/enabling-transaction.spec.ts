dd01a8a2ac827a58e54debf12cbe77a7b7708c60
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-14 18:28:24 +0500

    working "query builder > enabling transaction"

--- tests/functional/query-builder/enabling-transaction/enabling-transaction.spec.ts
+++ tests/functional/query-builder/enabling-transaction/enabling-transaction.spec.ts
@@ -1,16 +1,16 @@
 import "reflect-metadata";
-import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../utils/test-utils";
-import {Connection} from "../../../../src/connection/Connection";
+import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../../test/utils/test-utils";
+import {Connection} from "../../../../src";
 import {Post} from "./entity/Post";
 
 describe("query builder > enabling transaction", () => {
     
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({ __dirname }));
+    beforeAll(async () => connections = await createTestingConnections({ __dirname }));
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("should execute query in a transaction", () => Promise.all(connections.map(async connection => {
+    test("should execute query in a transaction", () => Promise.all(connections.map(async connection => {
 
         const post = new Post();
         post.title = "about transactions in query builder";
