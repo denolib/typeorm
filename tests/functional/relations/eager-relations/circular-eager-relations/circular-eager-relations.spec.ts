29326371c657adf3244a8dc14cfac33d30069207
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-14 16:36:38 +0500

    working (relations - circular-eager-relations)

--- tests/functional/relations/eager-relations/circular-eager-relations/circular-eager-relations.spec.ts
+++ tests/functional/relations/eager-relations/circular-eager-relations/circular-eager-relations.spec.ts
@@ -1,12 +1,11 @@
 import "reflect-metadata";
-import {Connection} from "../../../../../src/connection/Connection";
+import {Connection} from "../../../../../src";
 import {EntityMetadataValidator} from "../../../../../src/metadata-builder/EntityMetadataValidator";
 import {ConnectionMetadataBuilder} from "../../../../../src/connection/ConnectionMetadataBuilder";
-import {expect} from "chai";
 
 describe("relations > eager relations > circular eager relations", () => {
 
-    it("should throw error if eager: true is set on both sides of relationship", () => {
+    test("should throw error if eager: true is set on both sides of relationship", () => {
         const connection = new Connection({ // dummy connection options, connection won't be established anyway
             type: "mysql",
             host: "localhost",
@@ -18,7 +17,7 @@ describe("relations > eager relations > circular eager relations", () => {
         const connectionMetadataBuilder = new ConnectionMetadataBuilder(connection);
         const entityMetadatas = connectionMetadataBuilder.buildEntityMetadatas([__dirname + "/entity/*{.js,.ts}"]);
         const entityMetadataValidator = new EntityMetadataValidator();
-        expect(() => entityMetadataValidator.validateMany(entityMetadatas, connection.driver)).to.throw(Error);
+        expect(() => entityMetadataValidator.validateMany(entityMetadatas, connection.driver)).toThrow(Error);
     });
 
 });
