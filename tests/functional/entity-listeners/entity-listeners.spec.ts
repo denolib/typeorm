58b94af62b7ed655245bd5f2696fe6289958d2c4
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-15 13:55:01 +0500

    working "entity-listeners"

--- tests/functional/entity-listeners/entity-listeners.spec.ts
+++ tests/functional/entity-listeners/entity-listeners.spec.ts
@@ -1,18 +1,18 @@
 import {Connection} from "../../../src";
-import {closeTestingConnections, createTestingConnections} from "../../utils/test-utils";
+import {closeTestingConnections, createTestingConnections} from "../../../test/utils/test-utils";
 import {Post} from "./entity/Post";
 
 describe("entity-listeners", () => {
 
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
+    beforeAll(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
         dropSchema: true,
         schemaCreate: true
     }));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("beforeUpdate", () => Promise.all(connections.map(async connection => {
+    test("beforeUpdate", () => Promise.all(connections.map(async connection => {
         const post = new Post();
         post.title = "post title";
         post.text = "post text";
@@ -23,7 +23,7 @@ describe("entity-listeners", () => {
         await connection.manager.save(loadedPost);
 
         loadedPost = await connection.getRepository(Post).findOne(post.id);
-        loadedPost!.title.should.be.equal("post title");
+        expect(loadedPost!.title).toEqual("post title");
     })));
 
 });
