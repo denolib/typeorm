ce48e9ab7ab06741b578a313996de7000ce6a812
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-15 15:53:06 +0500

    working "metadata builder > MetadataArgsUtils"

--- tests/functional/metadata-builder/metadata-args-storage/metadata-args-storage.spec.ts
+++ tests/functional/metadata-builder/metadata-args-storage/metadata-args-storage.spec.ts
@@ -6,17 +6,17 @@ import {MetadataUtils} from "../../../../src/metadata-builder/MetadataUtils";
 
 describe("metadata builder > MetadataArgsUtils", () => {
 
-    it("getInheritanceTree", () => {
+    test("getInheritanceTree", () => {
         const inheritanceTree = MetadataUtils.getInheritanceTree(Post);
-        inheritanceTree.should.be.eql([
+        expect(inheritanceTree).toEqual([
             Post,
             ContentModule,
             Unit,
         ]);
     });
 
-    it("filterByTargetClasses", () => {
-        MetadataUtils.filterByTarget([
+    test("filterByTargetClasses", () => {
+        expect(MetadataUtils.filterByTarget([
             { },
             { target: undefined },
             { target: null },
@@ -25,12 +25,12 @@ describe("metadata builder > MetadataArgsUtils", () => {
             { target: Post },
             { target: ContentModule },
             { target: Unit },
-        ], [Post, Unit]).should.be.eql([
+        ], [Post, Unit])).toEqual([
             { target: Post },
             { target: Unit },
         ]);
 
-        MetadataUtils.filterByTarget([
+        expect(MetadataUtils.filterByTarget([
             { },
             { target: undefined },
             { target: null },
@@ -38,30 +38,11 @@ describe("metadata builder > MetadataArgsUtils", () => {
             { target: "" },
             { target: ContentModule },
             { target: Unit },
-        ], [Post, Unit]).should.be.eql([
+        ], [Post, Unit])).toEqual([
             { target: Unit },
         ]);
 
-        MetadataUtils.filterByTarget([
-            { },
-            { target: undefined },
-            { target: null },
-            { target: 1 },
-            { target: "" },
-            { target: ContentModule },
-            { target: Post },
-            { target: Unit },
-        ], [Post, Unit, ContentModule]).should.be.eql([
-            { target: ContentModule },
-            { target: Post },
-            { target: Unit },
-        ]);
-
-        MetadataUtils.filterByTarget([
-        ], [Post, Unit, ContentModule]).should.be.eql([
-        ]);
-
-        MetadataUtils.filterByTarget([
+        expect(MetadataUtils.filterByTarget([
             { },
             { target: undefined },
             { target: null },
@@ -70,7 +51,26 @@ describe("metadata builder > MetadataArgsUtils", () => {
             { target: ContentModule },
             { target: Post },
             { target: Unit },
-        ]).should.be.eql([
+        ], [Post, Unit, ContentModule])).toEqual([
+            { target: ContentModule },
+            { target: Post },
+            { target: Unit },
+        ]);
+
+        expect(MetadataUtils.filterByTarget([
+        ], [Post, Unit, ContentModule])).toEqual([
+        ]);
+
+        expect(MetadataUtils.filterByTarget([
+            { },
+            { target: undefined },
+            { target: null },
+            { target: 1 },
+            { target: "" },
+            { target: ContentModule },
+            { target: Post },
+            { target: Unit },
+        ])).toEqual([
             { },
             { target: undefined },
             { target: null },
