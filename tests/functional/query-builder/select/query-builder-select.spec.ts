924b27bd6f4bacdfcbf4f81f30ae080f6fc64416
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-15 10:53:56 +0500

    working "query builder > select"

--- tests/functional/query-builder/select/query-builder-select.spec.ts
+++ tests/functional/query-builder/select/query-builder-select.spec.ts
@@ -1,24 +1,23 @@
 import "reflect-metadata";
-import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../utils/test-utils";
-import {Connection} from "../../../../src/connection/Connection";
+import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../../test/utils/test-utils";
+import {Connection} from "../../../../src";
 import {Post} from "./entity/Post";
-import {expect} from "chai";
 
 describe("query builder > select", () => {
 
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
+    beforeAll(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
     }));
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("should append all entity mapped columns from main selection to select statement", () => Promise.all(connections.map(async connection => {
+    test("should append all entity mapped columns from main selection to select statement", () => Promise.all(connections.map(async connection => {
         const sql = connection.manager.createQueryBuilder(Post, "post")
             .disableEscaping()
             .getSql();
 
-        expect(sql).to.equal("SELECT post.id AS post_id, " +
+        expect(sql).toEqual("SELECT post.id AS post_id, " +
             "post.title AS post_title, " +
             "post.description AS post_description, " +
             "post.rating AS post_rating, " +
@@ -27,13 +26,13 @@ describe("query builder > select", () => {
             "FROM post post");
     })));
 
-    it("should append all entity mapped columns from both main selection and join selections to select statement", () => Promise.all(connections.map(async connection => {
+    test("should append all entity mapped columns from both main selection and join selections to select statement", () => Promise.all(connections.map(async connection => {
         const sql = connection.createQueryBuilder(Post, "post")
             .leftJoinAndSelect("category", "category")
             .disableEscaping()
             .getSql();
 
-        expect(sql).to.equal("SELECT post.id AS post_id, " +
+        expect(sql).toEqual("SELECT post.id AS post_id, " +
             "post.title AS post_title, " +
             "post.description AS post_description, " +
             "post.rating AS post_rating, " +
@@ -46,7 +45,7 @@ describe("query builder > select", () => {
             "FROM post post LEFT JOIN category category");
     })));
 
-    it("should append entity mapped columns from both main alias and join aliases to select statement", () => Promise.all(connections.map(async connection => {
+    test("should append entity mapped columns from both main alias and join aliases to select statement", () => Promise.all(connections.map(async connection => {
         const sql = connection.createQueryBuilder(Post, "post")
             .select("post.id")
             .addSelect("category.name")
@@ -54,45 +53,45 @@ describe("query builder > select", () => {
             .disableEscaping()
             .getSql();
 
-        expect(sql).to.equal("SELECT post.id AS post_id, " +
+        expect(sql).toEqual("SELECT post.id AS post_id, " +
             "category.name AS category_name " +
             "FROM post post LEFT JOIN category category");
     })));
 
-    it("should append entity mapped columns to select statement, if they passed as array", () => Promise.all(connections.map(async connection => {
+    test("should append entity mapped columns to select statement, if they passed as array", () => Promise.all(connections.map(async connection => {
         const sql = connection.createQueryBuilder(Post, "post")
             .select(["post.id", "post.title"])
             .disableEscaping()
             .getSql();
 
-        expect(sql).to.equal("SELECT post.id AS post_id, post.title AS post_title FROM post post");
+        expect(sql).toEqual("SELECT post.id AS post_id, post.title AS post_title FROM post post");
     })));
 
-    it("should append raw sql to select statement", () => Promise.all(connections.map(async connection => {
+    test("should append raw sql to select statement", () => Promise.all(connections.map(async connection => {
         const sql = connection.createQueryBuilder(Post, "post")
             .select("COUNT(*) as cnt")
             .disableEscaping()
             .getSql();
 
-        expect(sql).to.equal("SELECT COUNT(*) as cnt FROM post post");
+        expect(sql).toEqual("SELECT COUNT(*) as cnt FROM post post");
     })));
 
-    it("should append raw sql and entity mapped column to select statement", () => Promise.all(connections.map(async connection => {
+    test("should append raw sql and entity mapped column to select statement", () => Promise.all(connections.map(async connection => {
         const sql = connection.createQueryBuilder(Post, "post")
             .select(["COUNT(*) as cnt", "post.title"])
             .disableEscaping()
             .getSql();
 
-        expect(sql).to.equal("SELECT post.title AS post_title, COUNT(*) as cnt FROM post post");
+        expect(sql).toEqual("SELECT post.title AS post_title, COUNT(*) as cnt FROM post post");
     })));
 
-    it("should not create alias for selection, which is not entity mapped column", () => Promise.all(connections.map(async connection => {
+    test("should not create alias for selection, which is not entity mapped column", () => Promise.all(connections.map(async connection => {
         const sql = connection.createQueryBuilder(Post, "post")
             .select("post.name")
             .disableEscaping()
             .getSql();
 
-        expect(sql).to.equal("SELECT post.name FROM post post");
+        expect(sql).toEqual("SELECT post.name FROM post post");
     })));
 
 });
