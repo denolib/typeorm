5ed2404d9803f2945bb77f38ab031f616ac387d1
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-15 12:35:46 +0500

    working "ConnectionOptionsReader"

--- tests/functional/connection-options-reader/connection-options-reader.spec.ts
+++ tests/functional/connection-options-reader/connection-options-reader.spec.ts
@@ -1,41 +1,40 @@
-import {expect} from "chai";
-import {ConnectionOptions} from "../../../src/connection/ConnectionOptions";
-import {ConnectionOptionsReader} from "../../../src/connection/ConnectionOptionsReader";
+import {ConnectionOptions} from "../../../src";
+import {ConnectionOptionsReader} from "../../../src";
 
 describe("ConnectionOptionsReader", () => {
-  after(() => {
+  afterAll(() => {
     delete process.env.TYPEORM_CONNECTION;
     delete process.env.TYPEORM_DATABASE;
   });
 
-  it("properly loads config with entities specified", async () => {
+  test("properly loads config with entities specified", async () => {
     type EntititesList = Function[] | string[];
     const connectionOptionsReader = new ConnectionOptionsReader({ root: __dirname, configName: "configs/class-entities" });
     const options: ConnectionOptions = await connectionOptionsReader.get("test-conn");
-    expect(options.entities).to.be.an.instanceOf(Array);
+    expect(options.entities).toBeInstanceOf(Array);
     const entities: EntititesList = options.entities as EntititesList;
-    expect(entities.length).to.equal(1);
+    expect(entities.length).toEqual(1);
   });
 
-  it("properly loads sqlite in-memory/path config", async () => {
+  test("properly loads sqlite in-memory/path config", async () => {
     const connectionOptionsReader = new ConnectionOptionsReader({ root: __dirname, configName: "configs/sqlite-memory" });
     const inmemoryOptions: ConnectionOptions = await connectionOptionsReader.get("memory");
-    expect(inmemoryOptions.database).to.equal(":memory:");
+    expect(inmemoryOptions.database).toEqual(":memory:");
     const fileOptions: ConnectionOptions = await connectionOptionsReader.get("file");
-    expect(fileOptions.database).to.have.string("/test");
+    expect(fileOptions.database).toContain("/test");
   });
 
-  it("properly loads config with specified file path", async () => {
-    const connectionOptionsReader = new ConnectionOptionsReader({ root: __dirname, configName: "configs/test-path-config.js" });
+  test("properly loads config with specified file path", async () => {
+    const connectionOptionsReader = new ConnectionOptionsReader({ root: __dirname, configName: "configs/test-path-config.ts" });
     const fileOptions: ConnectionOptions = await connectionOptionsReader.get("file");
-    expect(fileOptions.database).to.have.string("/test-js");
+    expect(fileOptions.database).toContain("/test-js");
   });
 
   // TODO This test requires the configs/.env file be moved to the matching directory in build/compiled
-  it.skip("properly loads config from .env file", async () => {
+  test.skip("properly loads config from .env file", async () => {
     const connectionOptionsReader = new ConnectionOptionsReader({ root: __dirname, configName: "configs/.env" });
     const [ fileOptions ]: ConnectionOptions[] = await connectionOptionsReader.all();
-    expect(fileOptions.database).to.have.string("test-js");
-    expect(process.env.TYPEORM_DATABASE).to.equal("test-js");
+    expect(fileOptions.database).toContain("test-js");
+    expect(process.env.TYPEORM_DATABASE).toEqual("test-js");
   });
 });
