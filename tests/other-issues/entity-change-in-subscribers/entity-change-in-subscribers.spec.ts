e83f05fc73dec49ff8f31a67adf2bbb942732517
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-15 17:54:12 +0500

    working "other issues > entity change in subscribers should affect persistence"

--- tests/other-issues/entity-change-in-subscribers/entity-change-in-subscribers.spec.ts
+++ tests/other-issues/entity-change-in-subscribers/entity-change-in-subscribers.spec.ts
@@ -1,21 +1,20 @@
 import "reflect-metadata";
 import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../test/utils/test-utils";
-import {Connection} from "../../../src/connection/Connection";
+import {Connection} from "../../../src";
 import {Post} from "./entity/Post";
-import {expect} from "chai";
 import {PostCategory} from "./entity/PostCategory";
 
 describe("other issues > entity change in subscribers should affect persistence", () => {
 
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
+    beforeAll(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
         subscribers: [__dirname + "/subscriber/*{.js,.ts}"]
     }));
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("if entity was changed, subscriber should be take updated columns", () => Promise.all(connections.map(async function(connection) {
+    test("if entity was changed, subscriber should be take updated columns", () => Promise.all(connections.map(async function(connection) {
 
         const category1 = new PostCategory();
         category1.name = "category #1";
@@ -27,8 +26,8 @@ describe("other issues > entity change in subscribers should affect persistence"
 
         // check if it was inserted correctly
         const loadedPost = await connection.manager.findOne(Post);
-        expect(loadedPost).not.to.be.undefined;
-        loadedPost!.active.should.be.equal(false);
+        expect(loadedPost).not.toBeUndefined();
+        expect(loadedPost!.active).toEqual(false);
 
         // now update some property and let update subscriber trigger
         const category2 = new PostCategory();
@@ -41,9 +40,9 @@ describe("other issues > entity change in subscribers should affect persistence"
         // check if subscriber was triggered and entity was really taken changed columns in the subscriber
         const loadedUpdatedPost = await connection.manager.findOne(Post);
 
-        expect(loadedUpdatedPost).not.to.be.undefined;
-        expect(loadedUpdatedPost!.updatedColumns).to.equals(2);
-        expect(loadedUpdatedPost!.updatedRelations).to.equals(1);
+        expect(loadedUpdatedPost).not.toBeUndefined();
+        expect(loadedUpdatedPost!.updatedColumns).toEqual(2);
+        expect(loadedUpdatedPost!.updatedRelations).toEqual(1);
 
         await connection.manager.save(loadedPost!);
 
