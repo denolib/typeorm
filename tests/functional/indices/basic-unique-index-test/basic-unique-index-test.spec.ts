a3294ba1bc8c3c643c380eae1f5a0f78e1cbc45a
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-15 15:23:59 +0500

    working "indices > basic unique index test"

---
+++ tests/functional/indices/basic-unique-index-test/basic-unique-index-test.spec.ts
@@ -0,0 +1,26 @@
+import "reflect-metadata";
+import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../../test/utils/test-utils";
+import {Connection} from "../../../../src";
+import {Customer} from "./entity/Customer";
+
+describe("indices > basic unique index test", () => {
+
+    let connections: Connection[];
+    beforeAll(async () => connections = await createTestingConnections({
+        entities: [__dirname + "/entity/*{.js,.ts}"],
+    }));
+    beforeEach(() => reloadTestingDatabases(connections));
+    afterAll(() => closeTestingConnections(connections));
+
+    describe("unique index", function() {
+
+        test("should work without errors", () => Promise.all(connections.map(async connection => {
+            const customer = new Customer();
+            customer.nameEnglish = "Umed";
+            customer.nameHebrew = "Uma";
+            await connection.manager.save(customer);
+        })));
+
+    });
+
+});
