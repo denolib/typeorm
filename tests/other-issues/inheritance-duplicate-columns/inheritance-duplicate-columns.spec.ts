4de5c309831609c23dfa24e85da253640f980019
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-15 18:00:08 +0500

    working "other issues > double inheritance produces multiple duplicated columns"

--- tests/other-issues/inheritance-duplicate-columns/inheritance-duplicate-columns.spec.ts
+++ tests/other-issues/inheritance-duplicate-columns/inheritance-duplicate-columns.spec.ts
@@ -1,19 +1,18 @@
 import "reflect-metadata";
 import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../test/utils/test-utils";
-import {Connection} from "../../../src/connection/Connection";
+import {Connection} from "../../../src";
 import {Post} from "./entity/Post";
-import {expect} from "chai";
 
 describe("other issues > double inheritance produces multiple duplicated columns", () => {
 
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
+    beforeAll(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
     }));
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("should not produce duplicate columns", () => Promise.all(connections.map(async function(connection) {
+    test("should not produce duplicate columns", () => Promise.all(connections.map(async function(connection) {
 
         // insert a post
         const post = new Post();
@@ -22,8 +21,8 @@ describe("other issues > double inheritance produces multiple duplicated columns
 
         // check if it was inserted correctly
         const loadedPost = await connection.manager.findOne(Post);
-        expect(loadedPost).not.to.be.undefined;
-        loadedPost!.title.should.be.equal("hello");
+        expect(loadedPost).not.toBeUndefined();
+        expect(loadedPost!.title).toEqual("hello");
 
     })));
 
