67baf62fa5a8293cb04ac1c25c0cc565a800ffb9
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-15 17:58:44 +0500

    working "other issues > hydration performance"

--- tests/other-issues/hydrate-performance/hydrate-performance.spec.ts
+++ tests/other-issues/hydrate-performance/hydrate-performance.spec.ts
@@ -1,19 +1,19 @@
 import "reflect-metadata";
 import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../test/utils/test-utils";
-import {Connection} from "../../../src/connection/Connection";
+import {Connection} from "../../../src";
 import {Post} from "./entity/Post";
 
 describe("other issues > hydration performance", () => {
 
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
+    beforeAll(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
         enabledDrivers: ["mysql"]
     }));
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("if entity was changed in the listener, changed property should be updated in the db", () => Promise.all(connections.map(async function(connection) {
+    test("if entity was changed in the listener, changed property should be updated in the db", () => Promise.all(connections.map(async function(connection) {
 
         // insert few posts first
         const posts: Post[] = [];
@@ -25,13 +25,13 @@ describe("other issues > hydration performance", () => {
         // select them using raw sql
         // console.time("select using raw sql");
         const loadedRawPosts = await connection.manager.query("SELECT * FROM post");
-        loadedRawPosts.length.should.be.equal(100000);
+        expect(loadedRawPosts.length).toEqual(100000);
         // console.timeEnd("select using raw sql");
 
         // now select them using ORM
         // console.time("select using ORM");
         const loadedOrmPosts = await connection.manager.find(Post);
-        loadedOrmPosts.length.should.be.equal(100000);
+        expect(loadedOrmPosts.length).toEqual(100000);
         // console.timeEnd("select using ORM");
 
     })));
