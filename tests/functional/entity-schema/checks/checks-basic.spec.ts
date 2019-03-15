be7075e3e11c65156922f38800834dd31f7c2903
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-15 14:49:13 +0500

    working "entity-schema > checks"

--- tests/functional/entity-schema/checks/checks-basic.spec.ts
+++ tests/functional/entity-schema/checks/checks-basic.spec.ts
@@ -1,19 +1,19 @@
 import "reflect-metadata";
 import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../../test/utils/test-utils";
-import {Connection} from "../../../../src/connection/Connection";
+import {Connection} from "../../../../src";
 import {PersonSchema} from "./entity/Person";
 import {MysqlDriver} from "../../../../src/driver/mysql/MysqlDriver";
 
 describe("entity-schema > checks", () => {
 
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
+    beforeAll(async () => connections = await createTestingConnections({
         entities: [<any>PersonSchema],
     }));
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("should create a check constraints", () => Promise.all(connections.map(async connection => {
+    test("should create a check constraints", () => Promise.all(connections.map(async connection => {
         // Mysql does not support check constraints.
         if (connection.driver instanceof MysqlDriver)
             return;
@@ -22,7 +22,7 @@ describe("entity-schema > checks", () => {
         const table = await queryRunner.getTable("person");
         await queryRunner.release();
 
-        table!.checks.length.should.be.equal(2);
+        expect(table!.checks.length).toEqual(2);
 
     })));
 
