6eab1d73099468952f2df14eeaa6bdf4b74f7446
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-15 10:40:08 +0500

    working "query builder > relational query builder > add operation > one to many relation"

--- tests/functional/query-builder/relational/with-many/query-builder-relational-add-remove-one-to-many.spec.ts
+++ tests/functional/query-builder/relational/with-many/query-builder-relational-add-remove-one-to-many.spec.ts
@@ -2,19 +2,18 @@ import "reflect-metadata";
 import {Post} from "./entity/Post";
 import {Category} from "./entity/Category";
 import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../../../test/utils/test-utils";
-import {expect} from "chai";
-import {Connection} from "../../../../../src/connection/Connection";
+import {Connection} from "../../../../../src";
 
 describe("query builder > relational query builder > add operation > one to many relation", () => {
 
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
+    beforeAll(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
     }));
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("should add entity relation of a given entity by entity objects", () => Promise.all(connections.map(async connection => {
+    test("should add entity relation of a given entity by entity objects", () => Promise.all(connections.map(async connection => {
 
         const category1 = new Category();
         category1.name = "category #1";
@@ -47,13 +46,13 @@ describe("query builder > relational query builder > add operation > one to many
             .add(post1);
 
         let loadedPost1 = await connection.manager.findOne(Post, 1, { relations: ["category"] });
-        expect(loadedPost1!.category).to.be.eql({ id: 1, name: "category #1" });
+        expect(loadedPost1!.category).toEqual({ id: 1, name: "category #1" });
 
         let loadedPost2 = await connection.manager.findOne(Post, 2, { relations: ["category"] });
-        expect(loadedPost2!.category).to.be.null;
+        expect(loadedPost2!.category).toBeNull();
 
         let loadedPost3 = await connection.manager.findOne(Post, 3, { relations: ["category"] });
-        expect(loadedPost3!.category).to.be.null;
+        expect(loadedPost3!.category).toBeNull();
 
         await connection
             .createQueryBuilder()
@@ -62,16 +61,16 @@ describe("query builder > relational query builder > add operation > one to many
             .remove(post1);
 
         loadedPost1 = await connection.manager.findOne(Post, 1, { relations: ["category"] });
-        expect(loadedPost1!.category).to.be.null;
+        expect(loadedPost1!.category).toBeNull();
 
         loadedPost2 = await connection.manager.findOne(Post, 2, { relations: ["category"] });
-        expect(loadedPost2!.category).to.be.null;
+        expect(loadedPost2!.category).toBeNull();
 
         loadedPost3 = await connection.manager.findOne(Post, 3, { relations: ["category"] });
-        expect(loadedPost3!.category).to.be.null;
+        expect(loadedPost3!.category).toBeNull();
     })));
 
-    it("should add entity relation of a given entity by entity id", () => Promise.all(connections.map(async connection => {
+    test("should add entity relation of a given entity by entity id", () => Promise.all(connections.map(async connection => {
 
         const category1 = new Category();
         category1.name = "category #1";
@@ -104,13 +103,13 @@ describe("query builder > relational query builder > add operation > one to many
             .add(2); // post id
 
         let loadedPost1 = await connection.manager.findOne(Post, 1, { relations: ["category"] });
-        expect(loadedPost1!.category).to.be.null;
+        expect(loadedPost1!.category).toBeNull();
 
         let loadedPost2 = await connection.manager.findOne(Post, 2, { relations: ["category"] });
-        expect(loadedPost2!.category).to.be.eql({ id: 2, name: "category #2" });
+        expect(loadedPost2!.category).toEqual({ id: 2, name: "category #2" });
 
         let loadedPost3 = await connection.manager.findOne(Post, 3, { relations: ["category"] });
-        expect(loadedPost3!.category).to.be.null;
+        expect(loadedPost3!.category).toBeNull();
 
         await connection
             .createQueryBuilder()
@@ -119,16 +118,16 @@ describe("query builder > relational query builder > add operation > one to many
             .remove(2); // post id
 
         loadedPost1 = await connection.manager.findOne(Post, 1, { relations: ["category"] });
-        expect(loadedPost1!.category).to.be.null;
+        expect(loadedPost1!.category).toBeNull();
 
         loadedPost2 = await connection.manager.findOne(Post, 2, { relations: ["category"] });
-        expect(loadedPost2!.category).to.be.null;
+        expect(loadedPost2!.category).toBeNull();
 
         loadedPost3 = await connection.manager.findOne(Post, 3, { relations: ["category"] });
-        expect(loadedPost3!.category).to.be.null;
+        expect(loadedPost3!.category).toBeNull();
     })));
 
-    it("should add entity relation of a given entity by entity id map", () => Promise.all(connections.map(async connection => {
+    test("should add entity relation of a given entity by entity id map", () => Promise.all(connections.map(async connection => {
 
         const category1 = new Category();
         category1.name = "category #1";
@@ -161,13 +160,13 @@ describe("query builder > relational query builder > add operation > one to many
             .add({ id: 3 }); // post id
 
         let loadedPost1 = await connection.manager.findOne(Post, 1, { relations: ["category"] });
-        expect(loadedPost1!.category).to.be.null;
+        expect(loadedPost1!.category).toBeNull();
 
         let loadedPost2 = await connection.manager.findOne(Post, 2, { relations: ["category"] });
-        expect(loadedPost2!.category).to.be.null;
+        expect(loadedPost2!.category).toBeNull();
 
         let loadedPost3 = await connection.manager.findOne(Post, 3, { relations: ["category"] });
-        expect(loadedPost3!.category).to.be.eql({ id: 3, name: "category #3" });
+        expect(loadedPost3!.category).toEqual({ id: 3, name: "category #3" });
 
         await connection
             .createQueryBuilder()
@@ -176,16 +175,16 @@ describe("query builder > relational query builder > add operation > one to many
             .remove({ id: 3 }); // post id
 
         loadedPost1 = await connection.manager.findOne(Post, 1, { relations: ["category"] });
-        expect(loadedPost1!.category).to.be.null;
+        expect(loadedPost1!.category).toBeNull();
 
         loadedPost2 = await connection.manager.findOne(Post, 2, { relations: ["category"] });
-        expect(loadedPost2!.category).to.be.null;
+        expect(loadedPost2!.category).toBeNull();
 
         loadedPost3 = await connection.manager.findOne(Post, 3, { relations: ["category"] });
-        expect(loadedPost3!.category).to.be.null;
+        expect(loadedPost3!.category).toBeNull();
     })));
 
-    it("should add multiple entities into relation of a multiple entities", () => Promise.all(connections.map(async connection => {
+    test("should add multiple entities into relation of a multiple entities", () => Promise.all(connections.map(async connection => {
 
         const category1 = new Category();
         category1.name = "category #1";
@@ -218,13 +217,13 @@ describe("query builder > relational query builder > add operation > one to many
             .add([{ id: 1 }, { id: 3 }]); // posts
 
         let loadedPost1 = await connection.manager.findOne(Post, 1, { relations: ["category"] });
-        expect(loadedPost1!.category).to.be.eql({ id: 3, name: "category #3" });
+        expect(loadedPost1!.category).toEqual({ id: 3, name: "category #3" });
 
         let loadedPost2 = await connection.manager.findOne(Post, 2, { relations: ["category"] });
-        expect(loadedPost2!.category).to.be.null;
+        expect(loadedPost2!.category).toBeNull();
 
         let loadedPost3 = await connection.manager.findOne(Post, 3, { relations: ["category"] });
-        expect(loadedPost3!.category).to.be.eql({ id: 3, name: "category #3" });
+        expect(loadedPost3!.category).toEqual({ id: 3, name: "category #3" });
 
         await connection
             .createQueryBuilder()
@@ -233,16 +232,16 @@ describe("query builder > relational query builder > add operation > one to many
             .remove([{ id: 1 }, { id: 3 }]); // posts
 
         loadedPost1 = await connection.manager.findOne(Post, 1, { relations: ["category"] });
-        expect(loadedPost1!.category).to.be.null;
+        expect(loadedPost1!.category).toBeNull();
 
         loadedPost2 = await connection.manager.findOne(Post, 2, { relations: ["category"] });
-        expect(loadedPost2!.category).to.be.null;
+        expect(loadedPost2!.category).toBeNull();
 
         loadedPost3 = await connection.manager.findOne(Post, 3, { relations: ["category"] });
-        expect(loadedPost3!.category).to.be.null;
+        expect(loadedPost3!.category).toBeNull();
     })));
 
-    it("should handle addAndRemove method as well", () => Promise.all(connections.map(async connection => {
+    test("should handle addAndRemove method as well", () => Promise.all(connections.map(async connection => {
 
         const category1 = new Category();
         category1.name = "category #1";
@@ -276,7 +275,7 @@ describe("query builder > relational query builder > add operation > one to many
             .add(post2); // post
 
         let loadedPost1 = await connection.manager.findOne(Post, 2, { relations: ["category"] });
-        expect(loadedPost1!.category).to.be.eql({ id: 3, name: "category #3" });
+        expect(loadedPost1!.category).toEqual({ id: 3, name: "category #3" });
 
         // when nothing is specified nothing should be performed
         await connection
@@ -286,7 +285,7 @@ describe("query builder > relational query builder > add operation > one to many
             .addAndRemove([], []); // post
 
         loadedPost1 = await connection.manager.findOne(Post, 2, { relations: ["category"] });
-        expect(loadedPost1!.category).to.be.eql({ id: 3, name: "category #3" });
+        expect(loadedPost1!.category).toEqual({ id: 3, name: "category #3" });
 
         // now add and remove =)
         await connection
@@ -296,9 +295,9 @@ describe("query builder > relational query builder > add operation > one to many
             .addAndRemove([post1, post3], [post2]); // post
 
         const loadedCategory = await connection.manager.findOne(Category, 3, { relations: ["posts"] });
-        expect(loadedCategory!.posts).to.deep.include({ id: 1, title: "post #1" });
-        expect(loadedCategory!.posts).to.not.contain({ id: 2, title: "post #2" });
-        expect(loadedCategory!.posts).to.deep.include({ id: 3, title: "post #3" });
+        expect(loadedCategory!.posts).toContainEqual({ id: 1, title: "post #1" });
+        expect(loadedCategory!.posts).not.toContainEqual({ id: 2, title: "post #2" });
+        expect(loadedCategory!.posts).toContainEqual({ id: 3, title: "post #3" });
     })));
 
 });
