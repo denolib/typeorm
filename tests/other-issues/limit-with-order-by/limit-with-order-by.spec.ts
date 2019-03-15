553378bb5966d522573a7e021d7e3b8d57895802
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-15 18:04:17 +0500

    working "other issues > using limit in conjunction with order by"

--- tests/other-issues/limit-with-order-by/limit-with-order-by.spec.ts
+++ tests/other-issues/limit-with-order-by/limit-with-order-by.spec.ts
@@ -1,20 +1,19 @@
 import "reflect-metadata";
 import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../test/utils/test-utils";
-import {Connection} from "../../../src/connection/Connection";
+import {Connection} from "../../../src";
 import {Post} from "./entity/Post";
-import {expect} from "chai";
 import {Category} from "./entity/Category";
 
 describe("other issues > using limit in conjunction with order by", () => {
 
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
+    beforeAll(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
     }));
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("should persist successfully and return persisted entity", () => Promise.all(connections.map(async function(connection) {
+    test("should persist successfully and return persisted entity", () => Promise.all(connections.map(async function(connection) {
 
         // generate bulk array of posts with categories
         for (let i = 1; i <= 100; i++) {
@@ -40,18 +39,18 @@ describe("other issues > using limit in conjunction with order by", () => {
             .orderBy("post.id", "DESC")
             .getMany();
 
-        expect(loadedPosts1).not.to.be.undefined;
-        loadedPosts1.length.should.be.equal(10);
-        loadedPosts1[0].id.should.be.equal(100);
-        loadedPosts1[1].id.should.be.equal(99);
-        loadedPosts1[2].id.should.be.equal(98);
-        loadedPosts1[3].id.should.be.equal(97);
-        loadedPosts1[4].id.should.be.equal(96);
-        loadedPosts1[5].id.should.be.equal(95);
-        loadedPosts1[6].id.should.be.equal(94);
-        loadedPosts1[7].id.should.be.equal(93);
-        loadedPosts1[8].id.should.be.equal(92);
-        loadedPosts1[9].id.should.be.equal(91);
+        expect(loadedPosts1).not.toBeUndefined();
+        expect(loadedPosts1.length).toEqual(10);
+        expect(loadedPosts1[0].id).toEqual(100);
+        expect(loadedPosts1[1].id).toEqual(99);
+        expect(loadedPosts1[2].id).toEqual(98);
+        expect(loadedPosts1[3].id).toEqual(97);
+        expect(loadedPosts1[4].id).toEqual(96);
+        expect(loadedPosts1[5].id).toEqual(95);
+        expect(loadedPosts1[6].id).toEqual(94);
+        expect(loadedPosts1[7].id).toEqual(93);
+        expect(loadedPosts1[8].id).toEqual(92);
+        expect(loadedPosts1[9].id).toEqual(91);
 
     })));
 
