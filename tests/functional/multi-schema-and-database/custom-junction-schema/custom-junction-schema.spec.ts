11f22f54210264a25d2c9ce3e49a75f390b56d6c
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-15 16:38:11 +0500

    working "multi-schema-and-database > custom-junction-schema"

--- tests/functional/multi-schema-and-database/custom-junction-schema/custom-junction-schema.spec.ts
+++ tests/functional/multi-schema-and-database/custom-junction-schema/custom-junction-schema.spec.ts
@@ -1,35 +1,34 @@
 import "reflect-metadata";
-import {Connection} from "../../../../src/connection/Connection";
+import {Connection} from "../../../../src";
 import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../../test/utils/test-utils";
 import {Post} from "./entity/Post";
 import {Category} from "./entity/Category";
-import {expect} from "chai";
 
 describe("multi-schema-and-database > custom-junction-schema", () => {
 
     let connections: Connection[];
-    before(async () => {
+    beforeAll(async () => {
         connections = await createTestingConnections({
             entities: [Post, Category],
             enabledDrivers: ["mssql", "postgres"],
         });
     });
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("should correctly create tables when custom table schema used", () => Promise.all(connections.map(async connection => {
+    test("should correctly create tables when custom table schema used", () => Promise.all(connections.map(async connection => {
         const queryRunner = connection.createQueryRunner();
         const postTable = await queryRunner.getTable("yoman.post");
         const categoryTable = await queryRunner.getTable("yoman.category");
         const junctionMetadata = connection.getManyToManyMetadata(Post, "categories")!;
         const junctionTable = await queryRunner.getTable("yoman." + junctionMetadata.tableName);
         await queryRunner.release();
-        expect(postTable).not.to.be.undefined;
-        postTable!.name!.should.be.equal("yoman.post");
-        expect(categoryTable).not.to.be.undefined;
-        categoryTable!.name!.should.be.equal("yoman.category");
-        expect(junctionTable).not.to.be.undefined;
-        junctionTable!.name!.should.be.equal("yoman." + junctionMetadata.tableName);
+        expect(postTable).not.toBeUndefined();
+        expect(postTable!.name)!.toEqual("yoman.post");
+        expect(categoryTable).not.toBeUndefined();
+        expect(categoryTable!.name)!.toEqual("yoman.category");
+        expect(junctionTable).not.toBeUndefined();
+        expect(junctionTable!.name)!.toEqual("yoman." + junctionMetadata.tableName);
     })));
 
 });
