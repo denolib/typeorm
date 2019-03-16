46a858a93f1eddfc996049494a085ef90e497e46
Aleksander Ciesiolkiewicz <aleksander.ciesiolkiewicz@sdimedia.com>
2019-03-16 18:25:36 +0100

    Skip constructor when entity is loaded from database

---
+++ test/github-issues/1772/issue-1772.ts
@@ -0,0 +1,30 @@
+import "reflect-metadata";
+import {expect} from "chai";
+import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
+import {Connection} from "../../../src/connection/Connection";
+import {Post} from "./entity/Post";
+
+describe("github issues > #1772 Skip the constructor when entity is loaded from the database", () => {
+    let connections: Connection[];
+    before(async () => connections = await createTestingConnections({
+        entities: [Post],
+        dropSchema: true
+    }));
+    beforeEach(() => reloadTestingDatabases(connections));
+    after(() => closeTestingConnections(connections));
+
+    it("should not call the constructor", () => Promise.all(connections.map(async connection => {
+        const postRepository = connection.getRepository(Post);
+
+        // create and save a post first and check entity is initialized
+        const post = new Post("About columns");
+        await postRepository.save(post);
+
+        expect(post.initialized).to.be.true;
+
+        // check if entity loaded from database is not initialized by constructor
+        const loadedPost = await postRepository.findOne(1);
+        expect(loadedPost).to.be.instanceof(Post);
+        expect(loadedPost!.initialized).to.be.undefined;
+    })));
+});
