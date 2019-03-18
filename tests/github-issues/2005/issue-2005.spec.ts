936a20720b4a75630fe381282d7e51f5c6ed76e8
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-18 13:47:03 +0500

    working "github issues > #2005"

--- tests/github-issues/2005/issue-2005.spec.ts
+++ tests/github-issues/2005/issue-2005.spec.ts
@@ -1,22 +1,22 @@
 import {Connection} from "../../../src";
-import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
+import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../test/utils/test-utils";
 import {User} from "./entity/User";
 
 describe("github issues > #2005", () => {
 
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
+    beforeAll(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
         enabledDrivers: ["sqlite"]
     }));
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("should be able to find by boolean find", () => Promise.all(connections.map(async connection => {
+    test("should be able to find by boolean find", () => Promise.all(connections.map(async connection => {
         const user = new User();
         user.activated = true;
         await connection.manager.save(user);
-        user.activated.should.be.equal(true);
+        expect(user.activated).toEqual(true);
     })));
 
 });
