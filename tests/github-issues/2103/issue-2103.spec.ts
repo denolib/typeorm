fd6460c8bee4447b753d0c2a6a187920c43d6cbd
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-18 13:36:12 +0500

    working "github issues > #2103 query builder regression"

--- tests/github-issues/2103/issue-2103.spec.ts
+++ tests/github-issues/2103/issue-2103.spec.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata";
-import {createTestingConnections, closeTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
+import {createTestingConnections, closeTestingConnections, reloadTestingDatabases} from "../../../test/utils/test-utils";
 import {Connection} from "../../../src";
 import {Simple} from "./entity/Simple";
 import {Complex} from "./entity/Complex";
@@ -7,15 +7,15 @@ import {Complex} from "./entity/Complex";
 describe("github issues > #2103 query builder regression", () => {
 
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
+    beforeAll(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
         schemaCreate: true,
         dropSchema: true,
     }));
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("whereInIds should respect logical operator precedence > single simple primary key (in is used)", () => Promise.all(connections.map(async connection => {
+    test("whereInIds should respect logical operator precedence > single simple primary key (in is used)", () => Promise.all(connections.map(async connection => {
         const repository = connection.getRepository(Simple);
 
         const savedEntities = await repository.save([
@@ -25,7 +25,7 @@ describe("github issues > #2103 query builder regression", () => {
             repository.create({ x: 3 })
         ]);
 
-        savedEntities.length.should.be.equal(4); // check if they all are saved
+        expect(savedEntities.length).toEqual(4); // check if they all are saved
 
         const ids = savedEntities.map(entity => entity.id);
 
@@ -34,14 +34,14 @@ describe("github issues > #2103 query builder regression", () => {
             .andWhere("x = 1")
             .getMany();
 
-        entities.map(entity => entity.id).should.be.eql(
+        expect(entities.map(entity => entity.id)).toEqual(
             savedEntities
                 .filter(entity => entity.x === 1)
                 .map(entity => entity.id)
         );
     })));
 
-    it("whereInIds should respect logical operator precedence > multiple primary keys", () => Promise.all(connections.map(async connection => {
+    test("whereInIds should respect logical operator precedence > multiple primary keys", () => Promise.all(connections.map(async connection => {
         const repository = connection.getRepository(Complex);
 
         // sqlite does not support autoincrement for composite primary key, so we pass ids by ourselves here
@@ -52,7 +52,7 @@ describe("github issues > #2103 query builder regression", () => {
             repository.create({ id: 4, code: 1, x: 3 })
         ]);
 
-        savedEntities.length.should.be.equal(4); // check if they all are saved
+        expect(savedEntities.length).toEqual(4); // check if they all are saved
 
         const ids = savedEntities.map(entity => entity.id);
 
@@ -63,7 +63,7 @@ describe("github issues > #2103 query builder regression", () => {
             .andWhere("x = 1")
             .getMany();
 
-        entities.map(entity => entity.id).should.be.eql(
+        expect(entities.map(entity => entity.id)).toEqual(
             savedEntities
                 .filter(entity => entity.x === 1)
                 .map(entity => entity.id)
