3151b5028c1e7a81a0f9d30624539d707f76e8aa
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-18 13:08:29 +0500

    working "github issues > #2499 Postgres DELETE query result is useless"

--- tests/github-issues/2499/issue-2499.spec.ts
+++ tests/github-issues/2499/issue-2499.spec.ts
@@ -1,21 +1,20 @@
 import "reflect-metadata";
-import {createTestingConnections, closeTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
-import {Connection} from "../../../src/connection/Connection";
+import {createTestingConnections, closeTestingConnections, reloadTestingDatabases} from "../../../test/utils/test-utils";
+import {Connection} from "../../../src";
 import {Foo} from "./entity/Foo";
-import {expect} from "chai";
 
 describe("github issues > #2499 Postgres DELETE query result is useless", () => {
 
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
+    beforeAll(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
         schemaCreate: true,
         dropSchema: true,
     }));
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("should return correct number of affected rows for mysql, mariadb, postgres", () => Promise.all(connections.map(async connection => {
+    test("should return correct number of affected rows for mysql, mariadb, postgres", () => Promise.all(connections.map(async connection => {
         // skip test for sqlite because sqlite doesn't return any data on delete
         // sqljs -- the same
         // mongodb requires another test and it is also doesn't return correct number
@@ -32,6 +31,6 @@ describe("github issues > #2499 Postgres DELETE query result is useless", () => 
         // number 4 doesn't exist
         const result = await repo.delete([1, 2, 3, 4]);
 
-        expect(result.affected).to.eql(3);
+        expect(result.affected).toEqual(3);
     })));
 });
