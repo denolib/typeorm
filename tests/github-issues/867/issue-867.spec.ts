31af6a8db90d2ecb947b130573470dd48888ea54
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-18 18:56:20 +0500

    working "github issues > #867 result of `findAndCount` is wrong when apply `skip` and `take` option"

--- tests/github-issues/867/issue-867.spec.ts
+++ tests/github-issues/867/issue-867.spec.ts
@@ -1,19 +1,18 @@
 import "reflect-metadata";
-import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
-import {Connection} from "../../../src/connection/Connection";
+import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../test/utils/test-utils";
+import {Connection} from "../../../src";
 import {User} from "./entity/User";
-import {expect} from "chai";
 
 describe("github issues > #867 result of `findAndCount` is wrong when apply `skip` and `take` option", () => {
 
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
+    beforeAll(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
     }));
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("should work perfectly", () => Promise.all(connections.map(async connection => {
+    test("should work perfectly", () => Promise.all(connections.map(async connection => {
         const userRepository = connection.getRepository(User);
         const users = new Array(5).fill(0).map((n, i) => {
             const user = new User();
@@ -28,9 +27,9 @@ describe("github issues > #867 result of `findAndCount` is wrong when apply `ski
                 username: "ASC"
             }
         });
-        expect(totalCount).to.equal(5);
-        expect(foundUsers).to.have.lengthOf(2);
-        expect(foundUsers[0].username).to.equal("User_1");
+        expect(totalCount).toEqual(5);
+        expect(foundUsers).toHaveLength(2);
+        expect(foundUsers[0].username).toEqual("User_1");
     })));
 
 });
