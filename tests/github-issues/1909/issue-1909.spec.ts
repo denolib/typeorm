7a7bad7fbc7907854b78336b7c6430e89276cf58
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-18 14:24:18 +0500

    working "github issues > #1493 Error parsing pg connection string"

--- tests/github-issues/1909/issue-1909.spec.ts
+++ tests/github-issues/1909/issue-1909.spec.ts
@@ -1,8 +1,7 @@
 import { DriverUtils } from "../../../src/driver/DriverUtils";
-import { expect } from "chai";
 
 describe("github issues > #1493 Error parsing pg connection string", () => {
-    it("should parse url with empty password", () => {
+    test("should parse url with empty password", () => {
         const obj: any = {
             username: "usern@me",
             password: "",
@@ -13,11 +12,11 @@ describe("github issues > #1493 Error parsing pg connection string", () => {
         const url = `postgres://${obj.username}:@${obj.host}:${obj.port}/${obj.database}`;
         const options = DriverUtils.buildDriverOptions({url});
 
-        expect(options.username).to.eql(obj.username);
-        expect(options.password).to.eql(obj.password);
+        expect(options.username).toEqual(obj.username);
+        expect(options.password).toEqual(obj.password);
     });
 
-    it("should parse url without password", () => {
+    test("should parse url without password", () => {
         const obj: any = {
             username: "usern@me",
             password: "",
@@ -28,7 +27,7 @@ describe("github issues > #1493 Error parsing pg connection string", () => {
         const url = `postgres://${obj.username}@${obj.host}:${obj.port}/${obj.database}`;
         const options = DriverUtils.buildDriverOptions({url});
 
-        expect(options.username).to.eql(obj.username);
-        expect(options.password).to.eql(obj.password);
+        expect(options.username).toEqual(obj.username);
+        expect(options.password).toEqual(obj.password);
     });
 });
