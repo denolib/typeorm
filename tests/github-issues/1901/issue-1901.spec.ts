047becb849ff5d0dc31e66080661f21f09c5c0ad
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-18 14:25:33 +0500

    working "github issues > #1901 The correct way of adding `ON UPDATE CURRENT_TIMESTAMP` clause to timestamp column"

--- tests/github-issues/1901/issue-1901.spec.ts
+++ tests/github-issues/1901/issue-1901.spec.ts
@@ -1,13 +1,12 @@
 import "reflect-metadata";
-import {Connection} from "../../../src/connection/Connection";
-import {closeTestingConnections, createTestingConnections} from "../../utils/test-utils";
+import {Connection} from "../../../src";
+import {closeTestingConnections, createTestingConnections} from "../../../test/utils/test-utils";
 import {Post} from "./entity/Post";
-import {expect} from "chai";
 
 describe("github issues > #1901 The correct way of adding `ON UPDATE CURRENT_TIMESTAMP` clause to timestamp column", () => {
 
     let connections: Connection[];
-    before(async () => {
+    beforeAll(async () => {
         connections = await createTestingConnections({
             entities: [__dirname + "/entity/*{.js,.ts}"],
             enabledDrivers: ["mysql"],
@@ -15,19 +14,19 @@ describe("github issues > #1901 The correct way of adding `ON UPDATE CURRENT_TIM
             dropSchema: true,
         });
     });
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("should correctly create and change column with ON UPDATE expression", () => Promise.all(connections.map(async connection => {
+    test("should correctly create and change column with ON UPDATE expression", () => Promise.all(connections.map(async connection => {
         const queryRunner = connection.createQueryRunner();
         let table = await queryRunner.getTable("post");
-        table!.findColumnByName("updateAt")!.onUpdate!.should.be.equal("CURRENT_TIMESTAMP(3)");
+        expect(table!.findColumnByName("updateAt")!.onUpdate)!.toEqual("CURRENT_TIMESTAMP(3)");
 
         const metadata = connection.getMetadata(Post);
         metadata.findColumnWithPropertyName("updateAt")!.onUpdate = undefined;
         await connection.synchronize();
 
         table = await queryRunner.getTable("post");
-        expect(table!.findColumnByName("updateAt")!.onUpdate).to.be.undefined;
+        expect(table!.findColumnByName("updateAt")!.onUpdate).toBeUndefined();
 
     })));
 
