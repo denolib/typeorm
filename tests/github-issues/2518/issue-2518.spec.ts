b4ca026a2d0dfc4dae5d69757396d02a745708e8
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-18 13:06:23 +0500

    working "github issues > #2518 TreeRepository.findDescendantsTree does not load descendants when relationship id property exist"

--- tests/github-issues/2518/issue-2518.spec.ts
+++ tests/github-issues/2518/issue-2518.spec.ts
@@ -1,13 +1,12 @@
 import "reflect-metadata";
-import {createTestingConnections, closeTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
-import {Connection} from "../../../src/connection/Connection";
-import {expect} from "chai";
+import {createTestingConnections, closeTestingConnections, reloadTestingDatabases} from "../../../test/utils/test-utils";
+import {Connection} from "../../../src";
 import {File} from "./entity/file.entity";
 import {TreeRepository} from "../../../src";
 
 describe("github issues > #2518 TreeRepository.findDescendantsTree does not load descendants when relationship id property exist", () => {
     let connections: Connection[];
-    before(
+    beforeAll(
         async () =>
         (connections = await createTestingConnections({
             entities: [__dirname + "/entity/*{.js,.ts}"]
@@ -15,34 +14,34 @@ describe("github issues > #2518 TreeRepository.findDescendantsTree does not load
     );
 
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("should load descendants when findDescendantsTree is called for a tree entity", () =>
+    test("should load descendants when findDescendantsTree is called for a tree entity", () =>
         Promise.all(
             connections.map(async connection => {
                 const repo: TreeRepository<File> = connection.getTreeRepository(File);
                 const root: File = await repo.save({ id: 1, name: "root" } as File);
                 const child = await repo.save({ id: 2, name: "child", parent: root } as File);
-                expect(child.parentId).to.be.equal(1);
+                expect(child.parentId).toEqual(1);
                 const file: File | any = await repo.createQueryBuilder("file")
                     .where("file.id = :id", { id: 1 })
                     .getOne();
                 await repo.findDescendantsTree(file);
-                expect(file.children.length).to.be.greaterThan(0);
+                expect(file.children.length).toBeGreaterThan(0);
             })
         )
     );
 
-    it("should load descendants when findTrees are called", () =>
+    test("should load descendants when findTrees are called", () =>
         Promise.all(
             connections.map(async connection => {
                 const repo = connection.getTreeRepository(File);
                 const root: File = await repo.save({ id: 1, name: "root" } as File);
                 const child = await repo.save({ id: 2, name: "child", parent: root } as File);
-                expect(child.parentId).to.be.equal(1);
+                expect(child.parentId).toEqual(1);
                 const trees: File[] = await repo.findTrees();
-                expect(trees).to.be.an("array");
-                expect(trees[0].children.length).to.be.greaterThan(0);
+                expect(trees).toContain("array");
+                expect(trees[0].children.length).toBeGreaterThan(0);
             })
         )
     );
