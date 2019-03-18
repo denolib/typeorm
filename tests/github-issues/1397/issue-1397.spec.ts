048e62cebd13f514b39c560c7478a491beb43d83
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-18 17:31:33 +0500

    working "github issue > #1397 Spaces at the end of values are removed when inserting"

---
+++ tests/github-issues/1397/issue-1397.spec.ts
@@ -0,0 +1,28 @@
+import "reflect-metadata";
+import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../test/utils/test-utils";
+import {Connection} from "../../../src";
+import {Post} from "./entity/Post";
+
+describe("github issue > #1397 Spaces at the end of values are removed when inserting", () => {
+
+    let connections: Connection[] = [];
+    beforeAll(async () => connections = await createTestingConnections({
+        entities: [__dirname + "/entity/*{.js,.ts}"],
+        enabledDrivers: ["mysql"]
+    }));
+    beforeEach(() => reloadTestingDatabases(connections));
+    afterAll(() => closeTestingConnections(connections));
+
+    test("should not trim empty spaces when saving", () => Promise.all(connections.map(async connection => {
+
+        const post = new Post();
+        post.title = " About My Post   ";
+        await connection.manager.save(post);
+        expect(post.title).toEqual(" About My Post   ");
+
+        const loadedPost = await connection.manager.findOne(Post, { id: 1 });
+        expect(loadedPost).not.toBeUndefined();
+        expect(loadedPost!.title).toEqual(" About My Post   ");
+    })));
+
+});
