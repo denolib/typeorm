65d29a33d3f71e6429ed9d569309f4e068725318
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-18 16:09:55 +0500

    working "github issues > #1703 Many to Many with association table returns odd values."

--- tests/github-issues/1703/issue-1703.spec.ts
+++ tests/github-issues/1703/issue-1703.spec.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata";
-import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
+import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../test/utils/test-utils";
 import {Connection} from "../../../src";
 import {UserEntity} from "./entity/UserEntity";
 import {UserToOrganizationEntity} from "./entity/UserToOrganizationEntity";
@@ -8,14 +8,14 @@ import {OrganizationEntity} from "./entity/OrganizationEntity";
 describe("github issues > #1703 Many to Many with association table returns odd values.", () => {
 
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
+    beforeAll(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
         enabledDrivers: ["mysql"]
     }));
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("should work as expected", () => Promise.all(connections.map(async connection => {
+    test("should work as expected", () => Promise.all(connections.map(async connection => {
 
         const user1 = new UserEntity();
         const user2 = new UserEntity();
