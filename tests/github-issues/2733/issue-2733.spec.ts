f27200dc372f74e2908164389552d7aa2fa309ef
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-18 12:52:37 +0500

    working "github issues > #2733 should correctly handle function calls with upercase letters as default values"

--- tests/github-issues/2733/issue-2733.spec.ts
+++ tests/github-issues/2733/issue-2733.spec.ts
@@ -1,13 +1,12 @@
 import "reflect-metadata";
-import { createTestingConnections, closeTestingConnections, reloadTestingDatabases } from "../../utils/test-utils";
-import { Connection } from "../../../src/connection/Connection";
-import { expect } from "chai";
+import { createTestingConnections, closeTestingConnections, reloadTestingDatabases } from "../../../test/utils/test-utils";
+import { Connection } from "../../../src";
 
 describe("github issues > #2733 should correctly handle function calls with upercase letters as default values", () => {
 
     let connections: Connection[];
 
-    it("MSSQL, Sqljs, Sqlite", async () => {
+    test("MSSQL, Sqljs, Sqlite", async () => {
         connections = await createTestingConnections({
             entities: [__dirname + "/entity/MSSQLDummy{.js,.ts}"],
             schemaCreate: true,
@@ -18,12 +17,12 @@ describe("github issues > #2733 should correctly handle function calls with uper
         await Promise.all(connections.map(async connection => {
             const schemaBuilder = connection.driver.createSchemaBuilder();
             const syncQueries = await schemaBuilder.log();
-            expect(syncQueries.downQueries).to.be.eql([]);
-            expect(syncQueries.upQueries).to.be.eql([]);
+            expect(syncQueries.downQueries).toEqual([]);
+            expect(syncQueries.upQueries).toEqual([]);
         }));
         await closeTestingConnections(connections);
     });
-    it("Postgres", async () => {
+    test("Postgres", async () => {
         connections = await createTestingConnections({
             entities: [__dirname + "/entity/PostgresDummy{.js,.ts}"],
             schemaCreate: true,
@@ -34,8 +33,8 @@ describe("github issues > #2733 should correctly handle function calls with uper
         await Promise.all(connections.map(async connection => {
             const schemaBuilder = connection.driver.createSchemaBuilder();
             const syncQueries = await schemaBuilder.log();
-            expect(syncQueries.downQueries).to.be.eql([]);
-            expect(syncQueries.upQueries).to.be.eql([]);
+            expect(syncQueries.downQueries).toEqual([]);
+            expect(syncQueries.upQueries).toEqual([]);
         }));
         await closeTestingConnections(connections);
     });
