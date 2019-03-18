ffef2c1fa300475a8cd3b57db3cfed4a985205ae
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-18 11:34:23 +0500

    working "github issues > #3395 Transform.from does nothing when column is NULL"

--- tests/github-issues/3395/issue-3395.spec.ts
+++ tests/github-issues/3395/issue-3395.spec.ts
@@ -1,18 +1,18 @@
 import "reflect-metadata";
-import { closeTestingConnections, createTestingConnections, reloadTestingDatabases } from "../../utils/test-utils";
-import { Connection } from "../../../src/connection/Connection";
+import { closeTestingConnections, createTestingConnections, reloadTestingDatabases } from "../../../test/utils/test-utils";
+import { Connection } from "../../../src";
 import { Post } from "./entity/Post";
 
 describe("github issues > #3395 Transform.from does nothing when column is NULL", () => {
 
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
+    beforeAll(async () => connections = await createTestingConnections({
         entities: [Post]
     }));
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("should run transform from if column is null", () => Promise.all(connections.map(async function (connection) {
+    test("should run transform from if column is null", () => Promise.all(connections.map(async function (connection) {
 
         const post = new Post();
         post.id = 1;
@@ -20,7 +20,7 @@ describe("github issues > #3395 Transform.from does nothing when column is NULL"
 
         const loadedPost = await connection.getRepository(Post).findOne(1);
 
-        loadedPost!.text!.should.be.eq("This is null");
+        expect(loadedPost!.text)!.toEqual("This is null");
     })));
 
 });
