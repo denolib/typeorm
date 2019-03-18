a0a7d0430735c0f2c148343602f75048bc6fc2fd
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-18 12:46:32 +0500

    working "github issues > #2875 runMigrations() function is not returning a list of migrated files"

--- tests/github-issues/2875/issue-2875.spec.ts
+++ tests/github-issues/2875/issue-2875.spec.ts
@@ -1,11 +1,11 @@
 import "reflect-metadata";
-import {createTestingConnections, closeTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
-import {Connection} from "../../../src/connection/Connection";
+import {createTestingConnections, closeTestingConnections, reloadTestingDatabases} from "../../../test/utils/test-utils";
+import {Connection} from "../../../src";
 import {Migration} from "../../../src/migration/Migration";
 
 describe("github issues > #2875 runMigrations() function is not returning a list of migrated files", () => {
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
+    beforeAll(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
         migrations: [__dirname + "/migration/*.js"],
         enabledDrivers: ["postgres"],
@@ -13,12 +13,12 @@ describe("github issues > #2875 runMigrations() function is not returning a list
         dropSchema: true,
     }));
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("should be able to run all necessary migrations", () => Promise.all(connections.map(async connection => {
+    test("should be able to run all necessary migrations", () => Promise.all(connections.map(async connection => {
         const mymigr: Migration[] = await connection.runMigrations();
     
-        mymigr.length.should.be.equal(1);
-        mymigr[0].name.should.be.equal("InitUsers1530542855524");
+        expect(mymigr.length).toEqual(1);
+        expect(mymigr[0].name).toEqual("InitUsers1530542855524");
     })));
  });
