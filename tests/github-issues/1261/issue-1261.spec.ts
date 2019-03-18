74128fb3114adefcdac55bd699b91fab6fc0c949
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-18 17:47:36 +0500

    working "github issues > #1261 onDelete property on foreign key is not modified on sync"

--- tests/github-issues/1261/issue-1261.spec.ts
+++ tests/github-issues/1261/issue-1261.spec.ts
@@ -1,20 +1,20 @@
 import "reflect-metadata";
 import {CockroachDriver} from "../../../src/driver/cockroachdb/CockroachDriver";
-import {closeTestingConnections, createTestingConnections} from "../../utils/test-utils";
-import {Connection} from "../../../src/connection/Connection";
-import {BaseEntity} from "../../../src/repository/BaseEntity";
+import {closeTestingConnections, createTestingConnections} from "../../../test/utils/test-utils";
+import {Connection} from "../../../src";
+import {BaseEntity} from "../../../src";
 import {Bar} from "./entity/Bar";
 import {PromiseUtils} from "../../../src";
 
 describe("github issues > #1261 onDelete property on foreign key is not modified on sync", () => {
 
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
+    beforeAll(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
     }));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("should modify onDelete property on foreign key on sync", () => PromiseUtils.runInSequence(connections, async connection => {
+    test("should modify onDelete property on foreign key on sync", () => PromiseUtils.runInSequence(connections, async connection => {
 
         // TODO: issue in CRDB
         if (connection.driver instanceof CockroachDriver)
@@ -25,14 +25,14 @@ describe("github issues > #1261 onDelete property on foreign key is not modified
 
         const queryRunner = connection.createQueryRunner();
         let table = await queryRunner.getTable("bar");
-        table!.foreignKeys[0].onDelete!.should.be.equal("SET NULL");
+        expect(table!.foreignKeys[0].onDelete)!.toEqual("SET NULL");
 
         const metadata = connection.getMetadata(Bar);
         metadata.foreignKeys[0].onDelete = "CASCADE";
         await connection.synchronize();
 
         table = await queryRunner.getTable("bar");
-        table!.foreignKeys[0].onDelete!.should.be.equal("CASCADE");
+        expect(table!.foreignKeys[0].onDelete)!.toEqual("CASCADE");
 
         await queryRunner.release();
 
