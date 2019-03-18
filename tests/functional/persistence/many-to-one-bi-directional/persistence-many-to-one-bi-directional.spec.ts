71a4fbf97105bd51c03c1e002afe5faa3dc129c2
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-18 10:40:27 +0500

    working "persistence > many-to-one bi-directional relation"

--- tests/functional/persistence/many-to-one-bi-directional/persistence-many-to-one-bi-directional.spec.ts
+++ tests/functional/persistence/many-to-one-bi-directional/persistence-many-to-one-bi-directional.spec.ts
@@ -1,22 +1,21 @@
 import "reflect-metadata";
-import {Connection} from "../../../../src/connection/Connection";
+import {Connection} from "../../../../src";
 import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../../test/utils/test-utils";
 import {Post} from "./entity/Post";
 import {Category} from "./entity/Category";
-import {expect} from "chai";
 
 describe("persistence > many-to-one bi-directional relation", function() {
 
     let connections: Connection[];
-    before(async () => {
+    beforeAll(async () => {
         connections = await createTestingConnections({
             entities: [__dirname + "/entity/*{.js,.ts}"],
         });
     });
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("should save a category with a post attached", () => Promise.all(connections.map(async connection => {
+    test("should save a category with a post attached", () => Promise.all(connections.map(async connection => {
         const post = new Post(1, "Hello Post");
         await connection.manager.save(post);
 
@@ -25,22 +24,22 @@ describe("persistence > many-to-one bi-directional relation", function() {
         await connection.manager.save(category);
 
         const loadedCategory = await connection.manager.findOne(Category, 1, { relations: ["post"] });
-        expect(loadedCategory).not.to.be.undefined;
-        loadedCategory!.should.be.eql({ id: 1, name: "Hello Category", post: { id: 1, title: "Hello Post" } });
+        expect(loadedCategory).not.toBeUndefined();
+        expect(loadedCategory!).toEqual({ id: 1, name: "Hello Category", post: { id: 1, title: "Hello Post" } });
     })));
 
-    it("should save a category and a new post by cascades", () => Promise.all(connections.map(async connection => {
+    test("should save a category and a new post by cascades", () => Promise.all(connections.map(async connection => {
         const post = new Post(1, "Hello Post");
         const category = new Category(1, "Hello Category");
         category.post = post;
         await connection.manager.save(category);
 
         const loadedCategory = await connection.manager.findOne(Category, 1, { relations: ["post"] });
-        expect(loadedCategory).not.to.be.undefined;
-        loadedCategory!.should.be.eql({ id: 1, name: "Hello Category", post: { id: 1, title: "Hello Post" } });
+        expect(loadedCategory).not.toBeUndefined();
+        expect(loadedCategory!).toEqual({ id: 1, name: "Hello Category", post: { id: 1, title: "Hello Post" } });
     })));
 
-    it("should update exist post by cascades when category is saved", () => Promise.all(connections.map(async connection => {
+    test("should update exist post by cascades when category is saved", () => Promise.all(connections.map(async connection => {
         const post = new Post(1, "Hello Post");
         await connection.manager.save(post);
 
@@ -54,19 +53,19 @@ describe("persistence > many-to-one bi-directional relation", function() {
         await connection.manager.save(category);
 
         const loadedCategory1 = await connection.manager.findOne(Category, 1, { relations: ["post"] });
-        expect(loadedCategory1).not.to.be.undefined;
-        loadedCategory1!.should.be.eql({ id: 1, name: "Hello Category", post: { id: 1, title: "Updated post" } });
+        expect(loadedCategory1).not.toBeUndefined();
+        expect(loadedCategory1)!.toEqual({ id: 1, name: "Hello Category", post: { id: 1, title: "Updated post" } });
 
         // update post from loaded category
         (loadedCategory1!.post as Post).title = "Again Updated post";
         await connection.manager.save(loadedCategory1);
 
         const loadedCategory2 = await connection.manager.findOne(Category, 1, { relations: ["post"] });
-        expect(loadedCategory2).not.to.be.undefined;
-        loadedCategory2!.should.be.eql({ id: 1, name: "Hello Category", post: { id: 1, title: "Again Updated post" } });
+        expect(loadedCategory2).not.toBeUndefined();
+        expect(loadedCategory2)!.toEqual({ id: 1, name: "Hello Category", post: { id: 1, title: "Again Updated post" } });
     })));
 
-    it("should NOT remove exist post by cascades when category is saved without a post (post is set to undefined)", () => Promise.all(connections.map(async connection => {
+    test("should NOT remove exist post by cascades when category is saved without a post (post is set to undefined)", () => Promise.all(connections.map(async connection => {
         const post = new Post(1, "Hello Post");
         await connection.manager.save(post);
 
@@ -77,23 +76,23 @@ describe("persistence > many-to-one bi-directional relation", function() {
 
         // load and check if it was correctly saved
         const loadedCategory1 = await connection.manager.findOne(Category, 1, { relations: ["post"] });
-        expect(loadedCategory1).not.to.be.undefined;
-        loadedCategory1!.should.be.eql({ id: 1, name: "Hello Category", post: { id: 1, title: "Hello Post" } });
+        expect(loadedCategory1).not.toBeUndefined();
+        expect(loadedCategory1)!.toEqual({ id: 1, name: "Hello Category", post: { id: 1, title: "Hello Post" } });
 
         // remove post from loaded category
         loadedCategory1!.post = undefined;
         await connection.manager.save(loadedCategory1);
 
         const loadedCategory2 = await connection.manager.findOne(Category, 1, { relations: ["post"] });
-        expect(loadedCategory2).not.to.be.undefined;
-        loadedCategory2!.should.be.eql({ id: 1, name: "Hello Category", post: { id: 1, title: "Hello Post" } });
+        expect(loadedCategory2).not.toBeUndefined();
+        expect(loadedCategory2)!.toEqual({ id: 1, name: "Hello Category", post: { id: 1, title: "Hello Post" } });
 
         const loadedPost = await connection.manager.findOne(Post, 1);
-        expect(loadedPost).not.to.be.undefined;
-        loadedPost!.should.be.eql({ id: 1, title: "Hello Post" });
+        expect(loadedPost).not.toBeUndefined();
+        expect(loadedPost)!.toEqual({ id: 1, title: "Hello Post" });
     })));
 
-    it("should unset exist post when its set to null", () => Promise.all(connections.map(async connection => {
+    test("should unset exist post when its set to null", () => Promise.all(connections.map(async connection => {
         const post = new Post(1, "Hello Post");
         await connection.manager.save(post);
 
@@ -103,19 +102,19 @@ describe("persistence > many-to-one bi-directional relation", function() {
         await connection.manager.save(category);
 
         const loadedCategory1 = await connection.manager.findOne(Category, 1, { relations: ["post"] });
-        expect(loadedCategory1).not.to.be.undefined;
-        loadedCategory1!.should.be.eql({ id: 1, name: "Hello Category", post: { id: 1, title: "Hello Post" } });
+        expect(loadedCategory1).not.toBeUndefined();
+        expect(loadedCategory1)!.toEqual({ id: 1, name: "Hello Category", post: { id: 1, title: "Hello Post" } });
 
         // remove post from loaded category
         loadedCategory1!.post = null;
         await connection.manager.save(loadedCategory1);
 
         const loadedCategory2 = await connection.manager.findOne(Category, 1, { relations: ["post"] });
-        expect(loadedCategory2).not.to.be.undefined;
-        loadedCategory2!.should.be.eql({ id: 1, name: "Hello Category", post: null });
+        expect(loadedCategory2).not.toBeUndefined();
+        expect(loadedCategory2)!.toEqual({ id: 1, name: "Hello Category", post: null });
     })));
 
-    it("should set category's post to NULL when post is removed from the database (database ON DELETE)", () => Promise.all(connections.map(async connection => {
+    test("should set category's post to NULL when post is removed from the database (database ON DELETE)", () => Promise.all(connections.map(async connection => {
         const post = new Post(1, "Hello Post");
         await connection.manager.save(post);
 
@@ -125,20 +124,20 @@ describe("persistence > many-to-one bi-directional relation", function() {
         await connection.manager.save(category);
 
         const loadedCategory1 = await connection.manager.findOne(Category, 1, { relations: ["post"] });
-        expect(loadedCategory1).not.to.be.undefined;
-        loadedCategory1!.should.be.eql({ id: 1, name: "Hello Category", post: { id: 1, title: "Hello Post" } });
+        expect(loadedCategory1).not.toBeUndefined();
+        expect(loadedCategory1)!.toEqual({ id: 1, name: "Hello Category", post: { id: 1, title: "Hello Post" } });
 
         // remove post from loaded category
         await connection.manager.remove(post);
 
         // now lets load category and make sure post isn't set there
         const loadedCategory2 = await connection.manager.findOne(Category, 1, { relations: ["post"] });
-        expect(loadedCategory2).not.to.be.undefined;
-        loadedCategory2!.should.be.eql({ id: 1, name: "Hello Category", post: null });
+        expect(loadedCategory2).not.toBeUndefined();
+        expect(loadedCategory2)!.toEqual({ id: 1, name: "Hello Category", post: null });
     })));
 
 
-    it("should work when relation id is directly set into relation (without related object)", () => Promise.all(connections.map(async connection => {
+    test("should work when relation id is directly set into relation (without related object)", () => Promise.all(connections.map(async connection => {
 
         const post1 = new Post(1, "Hello Post #1");
         await connection.manager.save(post1);
@@ -153,8 +152,8 @@ describe("persistence > many-to-one bi-directional relation", function() {
 
         // check if category is saved with post set
         const loadedCategory1 = await connection.manager.findOne(Category, 1, { relations: ["post"] });
-        expect(loadedCategory1).not.to.be.undefined;
-        loadedCategory1!.should.be.eql({ id: 1, name: "Hello Category", post: { id: 1, title: "Hello Post #1" } });
+        expect(loadedCategory1).not.toBeUndefined();
+        expect(loadedCategory1)!.toEqual({ id: 1, name: "Hello Category", post: { id: 1, title: "Hello Post #1" } });
 
         // now update a category with another post
         category.post = 2;
@@ -162,8 +161,8 @@ describe("persistence > many-to-one bi-directional relation", function() {
 
         // and check again if category is saved with new post
         const loadedCategory2 = await connection.manager.findOne(Category, 1, { relations: ["post"] });
-        expect(loadedCategory2).not.to.be.undefined;
-        loadedCategory2!.should.be.eql({ id: 1, name: "Hello Category", post: { id: 2, title: "Hello Post #2" } });
+        expect(loadedCategory2).not.toBeUndefined();
+        expect(loadedCategory2)!.toEqual({ id: 1, name: "Hello Category", post: { id: 2, title: "Hello Post #2" } });
     })));
 
 });
