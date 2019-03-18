612c0fff81d93a607fe8e9e9c975557ee125b2bd
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-18 11:59:45 +0500

    working "github issues > #3256 wrong subscriber methods being called"

--- tests/github-issues/3256/issue-3256.spec.ts
+++ tests/github-issues/3256/issue-3256.spec.ts
@@ -1,34 +1,34 @@
 import "reflect-metadata";
-import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
-import {Connection} from "../../../src/connection/Connection";
+import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../test/utils/test-utils";
+import {Connection} from "../../../src";
 import {Post} from "./entity/Post";
 
 describe("github issues > #3256 wrong subscriber methods being called", () => {
 
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
+    beforeAll(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
         subscribers: [__dirname + "/subscriber/*{.js,.ts}"],
     }));
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("if entity was changed, subscriber should be take updated columns", () => Promise.all(connections.map(async function(connection) {
+    test("if entity was changed, subscriber should be take updated columns", () => Promise.all(connections.map(async function(connection) {
 
         const post = new Post();
         post.id = 1;
         post.title = "hello world";
         await connection.manager.save(post);
 
-        post.inserted.should.be.equal(true);
-        post.updated.should.be.equal(false);
+        expect(post.inserted).toEqual(true);
+        expect(post.updated).toEqual(false);
 
         const loadedPost = await connection.getRepository(Post).findOne(1);
         loadedPost!.title = "updated world";
         await connection.manager.save(loadedPost);
 
-        loadedPost!.inserted.should.be.equal(false);
-        loadedPost!.updated.should.be.equal(true);
+        expect(loadedPost!.inserted).toEqual(false);
+        expect(loadedPost!.updated).toEqual(true);
 
     })));
 
