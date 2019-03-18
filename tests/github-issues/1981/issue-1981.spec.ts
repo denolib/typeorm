f1b440109fc6f27c0e390fa6df7733466ce842c3
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-18 14:13:22 +0500

    working "github issues > #1981 Boolean values not casted properly when used in .find() condition"

--- tests/github-issues/1981/issue-1981.spec.ts
+++ tests/github-issues/1981/issue-1981.spec.ts
@@ -1,24 +1,24 @@
 import {Connection} from "../../../src";
-import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
+import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../test/utils/test-utils";
 import {Product} from "./entity/Product";
 
 describe("github issues > #1981 Boolean values not casted properly when used in .find() condition", () => {
 
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
+    beforeAll(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
         enabledDrivers: ["sqlite"],
     }));
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("should be able to find by boolean find", () => Promise.all(connections.map(async connection => {
+    test("should be able to find by boolean find", () => Promise.all(connections.map(async connection => {
         const product = new Product();
         product.liked = true;
         await connection.manager.save(product);
 
         const loadedProduct = await connection.manager.findOne(Product, { liked: true });
-        loadedProduct!.liked.should.be.equal(true);
+        expect(loadedProduct!.liked).toEqual(true);
     })));
 
 });
