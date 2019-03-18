9be796e3b1fc189c785ec32a1f72279477d1841a
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-18 16:04:09 +0500

    working "github issues > #1720 Listener not invoked when relation loaded through getter"

--- tests/github-issues/1720/issue-1720.spec.ts
+++ tests/github-issues/1720/issue-1720.spec.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata";
-import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
+import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../test/utils/test-utils";
 import {Connection} from "../../../src";
 import {Post} from "./entity/Post";
 import {Category} from "./entity/Category";
@@ -7,14 +7,14 @@ import {Category} from "./entity/Category";
 describe("github issues > #1720 Listener not invoked when relation loaded through getter", () => {
 
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
+    beforeAll(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
         enabledDrivers: ["mysql"]
     }));
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("should work as expected", () => Promise.all(connections.map(async connection => {
+    test("should work as expected", () => Promise.all(connections.map(async connection => {
         const category1 = new Category();
         category1.name = "cat #1";
         await connection.manager.save(category1);
@@ -29,8 +29,8 @@ describe("github issues > #1720 Listener not invoked when relation loaded throug
         await connection.manager.save(post1);
 
         const loadedPost = await connection.manager.findOne(Post, { relations: ["categories"] });
-        loadedPost!.categories[0].loaded.should.be.equal(true);
-        loadedPost!.categories[1].loaded.should.be.equal(true);
+        expect(loadedPost!.categories[0].loaded).toEqual(true);
+        expect(loadedPost!.categories[1].loaded).toEqual(true);
     })));
 
 });
