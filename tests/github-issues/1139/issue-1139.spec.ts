12e4cc7af6a3fc7de7b8b24b3da89ed790d5e4d6
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-18 18:05:18 +0500

    working "github issues > #1139 mysql primary generated uuid ER_TOO_LONG_KEY"

---
+++ tests/github-issues/1139/issue-1139.spec.ts
@@ -0,0 +1,14 @@
+import "reflect-metadata";
+import { createTestingConnections, closeTestingConnections } from "../../../test/utils/test-utils";
+import { Connection } from "../../../src";
+
+describe("github issues > #1139 mysql primary generated uuid ER_TOO_LONG_KEY", () => {
+    let connections: Connection[];
+    afterAll(() => closeTestingConnections(connections));
+    test("correctly create primary generated uuid column", async () => connections = await createTestingConnections({
+        entities: [__dirname + "/entity/*{.js,.ts}"],
+        enabledDrivers: ["mysql"],
+        schemaCreate: true,
+        dropSchema: true,
+    }));
+});
