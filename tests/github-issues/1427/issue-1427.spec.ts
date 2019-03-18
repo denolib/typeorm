56afc95132fe776e7abc83ba88c5e1e3c643bfc8
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-18 17:28:15 +0500

    working "github issues > #1427 precision and scale column types with errant behavior"

---
+++ tests/github-issues/1427/issue-1427.spec.ts
@@ -0,0 +1,28 @@
+import "reflect-metadata";
+import {Connection} from "../../../src";
+import {closeTestingConnections, createTestingConnections} from "../../../test/utils/test-utils";
+
+describe("github issues > #1427 precision and scale column types with errant behavior", () => {
+
+    let connections: Connection[];
+    beforeAll(async () => {
+        connections = await createTestingConnections({
+            entities: [__dirname + "/entity/*{.js,.ts}"],
+            enabledDrivers: ["mysql"],
+            schemaCreate: true,
+            dropSchema: true,
+        });
+    });
+    afterAll(() => closeTestingConnections(connections));
+
+    test("should correctly create column with precision and scale", () => Promise.all(connections.map(async connection => {
+        const queryRunner = connection.createQueryRunner();
+        let table = await queryRunner.getTable("post");
+        await queryRunner.release();
+
+        expect(table!.findColumnByName("qty")!.type).toEqual("decimal");
+        expect(table!.findColumnByName("qty")!.precision)!.toEqual(10);
+        expect(table!.findColumnByName("qty")!.scale)!.toEqual(6);
+    })));
+
+});
