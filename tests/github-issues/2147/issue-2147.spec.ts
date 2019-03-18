a6e6bdf5f4f365efa2d39a2e904a708dcdde56fa
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-18 13:30:26 +0500

    working "github issues > #2147 Lazy load JoinColumn with multiple columns name property is ignored for second reference column"

--- tests/github-issues/2147/issue-2147.spec.ts
+++ tests/github-issues/2147/issue-2147.spec.ts
@@ -1,22 +1,21 @@
 import "reflect-metadata";
-import {createTestingConnections, closeTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
-import {Connection} from "../../../src/connection/Connection";
-import {expect} from "chai";
+import {createTestingConnections, closeTestingConnections, reloadTestingDatabases} from "../../../test/utils/test-utils";
+import {Connection} from "../../../src";
 import {User} from "./entity/User";
 
 // TODO: wrong test
-describe.skip("github issues > #2147 Lazy load JoinColumn with multiple columns name property is ignored for second reference column", () => {
+describe("github issues > #2147 Lazy load JoinColumn with multiple columns name property is ignored for second reference column", () => {
 
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
+    beforeAll(async () => connections = await createTestingConnections({
          entities: [__dirname + "/entity/*{.js,.ts}"],
          schemaCreate: true,
          dropSchema: true,
     }));
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("should create multiple column join for lazy loading relationship", () => {
+    test("should create multiple column join for lazy loading relationship", () => {
         return Promise.all(connections.map(async connection => {
             // tests go here
             const username = "user name";
@@ -31,7 +30,7 @@ describe.skip("github issues > #2147 Lazy load JoinColumn with multiple columns 
 
             const updatedBy = await users[0].updatedBy;
 
-            return expect(updatedBy.name).to.be.equal(username);
+            return expect(updatedBy.name).toEqual(username);
 
         }));
     });
