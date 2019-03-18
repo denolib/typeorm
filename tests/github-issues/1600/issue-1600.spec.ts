15e1523d0c43c5a98f3cb5373e116c3a7ae57abc
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-18 17:07:59 +0500

    working "github issues > #1600 Postgres: QueryBuilder insert with Postgres array type bug"

--- tests/github-issues/1600/issue-1600.spec.ts
+++ tests/github-issues/1600/issue-1600.spec.ts
@@ -1,19 +1,19 @@
 import "reflect-metadata";
-import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
-import {Connection} from "../../../src/connection/Connection";
+import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../test/utils/test-utils";
+import {Connection} from "../../../src";
 import {User} from "./entity/User";
 
 describe("github issues > #1600 Postgres: QueryBuilder insert with Postgres array type bug", () => {
 
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
+    beforeAll(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
         enabledDrivers: ["postgres"]
     }));
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("should insert successfully using save method", () => Promise.all(connections.map(async connection => {
+    test("should insert successfully using save method", () => Promise.all(connections.map(async connection => {
 
         const users: User[] = [];
         for (let i = 0; i < 10; i++) {
@@ -27,24 +27,24 @@ describe("github issues > #1600 Postgres: QueryBuilder insert with Postgres arra
             .createQueryBuilder(User, "user")
             .getMany();
 
-        loadedUsers1.length.should.be.equal(10);
+        expect(loadedUsers1.length).toEqual(10);
 
         const loadedUsers2 = await connection
             .createQueryBuilder(User, "user")
             .where("user.id IN (:...ids)", { ids: [1, 2, 3, 15] })
             .getMany();
 
-        loadedUsers2.length.should.be.equal(3);
+        expect(loadedUsers2.length).toEqual(3);
 
         const loadedUsers3 = await connection
             .createQueryBuilder(User, "user")
             .where("user.id = ANY(:ids)", { ids: [1, 2, 15] })
             .getMany();
 
-        loadedUsers3.length.should.be.equal(2);
+        expect(loadedUsers3.length).toEqual(2);
     })));
 
-    it("should insert successfully using insert method", () => Promise.all(connections.map(async connection => {
+    test("should insert successfully using insert method", () => Promise.all(connections.map(async connection => {
 
         const users: User[] = [];
         for (let i = 0; i < 10; i++) {
@@ -64,7 +64,7 @@ describe("github issues > #1600 Postgres: QueryBuilder insert with Postgres arra
             .createQueryBuilder(User, "user")
             .getMany();
 
-        loadedUsers.length.should.be.equal(10);
+        expect(loadedUsers.length).toEqual(10);
     })));
 
 });
