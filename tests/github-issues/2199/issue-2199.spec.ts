5c4ed52be7fff2148eae8b69bd9e10db46e96e17
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-18 13:29:14 +0500

    working "github issues > #2199 - Inserting value for @PrimaryGeneratedColumn() for mysql"

--- tests/github-issues/2199/issue-2199.spec.ts
+++ tests/github-issues/2199/issue-2199.spec.ts
@@ -1,13 +1,12 @@
-import { expect } from "chai";
 import "reflect-metadata";
-import { Connection } from "../../../src/connection/Connection";
-import { closeTestingConnections, createTestingConnections, reloadTestingDatabases } from "../../utils/test-utils";
+import { Connection } from "../../../src";
+import { closeTestingConnections, createTestingConnections, reloadTestingDatabases } from "../../../test/utils/test-utils";
 import { Bar } from "./entity/Bar";
 
 describe("github issues > #2199 - Inserting value for @PrimaryGeneratedColumn() for mysql", () => {
 
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
+    beforeAll(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
         enabledDrivers: ["mysql", "mariadb"],
         schemaCreate: true,
@@ -15,16 +14,16 @@ describe("github issues > #2199 - Inserting value for @PrimaryGeneratedColumn() 
     }));
 
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("should allow to explicitly insert primary key value", () => Promise.all(connections.map(async connection => {
+    test("should allow to explicitly insert primary key value", () => Promise.all(connections.map(async connection => {
 
         const firstBarQuery =  connection.manager.create(Bar, {
              id: 10,
             description: "forced id value"
         });
         const firstBar = await connection.manager.save(firstBarQuery);
-        expect(firstBar.id).to.eql(10);
+        expect(firstBar.id).toEqual(10);
 
         // Mysql stores and tracks AUTO_INCREMENT value for each table,
         // If the new value is higher than the current maximum value or not specified (use DEFAULT),
@@ -33,7 +32,7 @@ describe("github issues > #2199 - Inserting value for @PrimaryGeneratedColumn() 
             description: "default next id value"
         });
         const secondBar = await connection.manager.save(secondBarQuery);
-        expect(secondBar.id).to.eql(firstBarQuery.id + 1);
+        expect(secondBar.id).toEqual(firstBarQuery.id + 1);
 
         // If the new value is lower than the current maximum value,
         // the AUTO_INCREMENT value remains unchanged.
@@ -42,6 +41,6 @@ describe("github issues > #2199 - Inserting value for @PrimaryGeneratedColumn() 
             description: "lower forced id value"
         });
         const thirdBar = await connection.manager.save(thirdBarQuery);
-        expect(thirdBar.id).to.eql(5);
+        expect(thirdBar.id).toEqual(5);
     })));
 });
