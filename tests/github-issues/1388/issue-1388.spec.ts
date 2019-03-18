8052bad91b9dab3be0f2e2f005846db8c8e66813
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-18 17:33:48 +0500

    working "github issues > #1388 nullable: true dons't output 'NULL' in mysql"

--- tests/github-issues/1388/issue-1388.spec.ts
+++ tests/github-issues/1388/issue-1388.spec.ts
@@ -1,11 +1,11 @@
 import "reflect-metadata";
-import {Connection} from "../../../src/connection/Connection";
-import {closeTestingConnections, createTestingConnections} from "../../utils/test-utils";
+import {Connection} from "../../../src";
+import {closeTestingConnections, createTestingConnections} from "../../../test/utils/test-utils";
 
 describe("github issues > #1388 nullable: true dons't output 'NULL' in mysql", () => {
 
     let connections: Connection[];
-    before(async () => {
+    beforeAll(async () => {
         connections = await createTestingConnections({
             entities: [__dirname + "/entity/*{.js,.ts}"],
             enabledDrivers: ["mysql"],
@@ -13,12 +13,12 @@ describe("github issues > #1388 nullable: true dons't output 'NULL' in mysql", (
             dropSchema: true,
         });
     });
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("should correctly create nullable column", () => Promise.all(connections.map(async connection => {
+    test("should correctly create nullable column", () => Promise.all(connections.map(async connection => {
         const queryRunner = connection.createQueryRunner();
         const table = await queryRunner.getTable("post");
-        table!.findColumnByName("createdAt")!.isNullable.should.be.true;
+        expect(table!.findColumnByName("createdAt")!.isNullable).toBeTruthy();
         await queryRunner.release();
     })));
 
