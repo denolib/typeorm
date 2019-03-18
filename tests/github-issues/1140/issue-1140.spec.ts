8e68abd8efbce400e51f824b91c93aee3f39aa15
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-18 18:03:48 +0500

    working "github issues > #1140 timestamp column and value transformer causes TypeError"

--- tests/github-issues/1140/issue-1140.spec.ts
+++ tests/github-issues/1140/issue-1140.spec.ts
@@ -1,21 +1,20 @@
 import "reflect-metadata";
-import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
-import {Connection} from "../../../src/connection/Connection";
+import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../test/utils/test-utils";
+import {Connection} from "../../../src";
 import {Post} from "./entity/Post";
-import {expect} from "chai";
 
 describe("github issues > #1140 timestamp column and value transformer causes TypeError", () => {
 
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
+    beforeAll(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
         schemaCreate: true,
         dropSchema: true,
     }));
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("correctly store/load timestamp columns", () => Promise.all(connections.map(async connection => {
+    test("correctly store/load timestamp columns", () => Promise.all(connections.map(async connection => {
         const date = new Date();
         date.setMilliseconds(0); // Because some databases don't have millisecond resolution
         const dateNumber = date.getTime();
@@ -25,7 +24,7 @@ describe("github issues > #1140 timestamp column and value transformer causes Ty
         await connection.manager.save(post);
 
         const loadedPosts = await connection.manager.find(Post);
-        loadedPosts.length.should.be.equal(1);
-        expect(loadedPosts[0].ts).to.be.equal(dateNumber);
+        expect(loadedPosts.length).toEqual(1);
+        expect(loadedPosts[0].ts).toEqual(dateNumber);
     })));
 });
