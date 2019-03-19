1cb558b15480912fa517c784f8e155edcf624a62
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-19 11:19:23 +0500

    working "github issues > #736 ClosureEntity should set (composite) primary/unique key in the closure table"

--- tests/github-issues/736/issue-736.spec.ts
+++ tests/github-issues/736/issue-736.spec.ts
@@ -1,24 +1,24 @@
 import "reflect-metadata";
-import {Connection} from "../../../src/connection/Connection";
-import {closeTestingConnections, createTestingConnections} from "../../utils/test-utils";
+import {Connection} from "../../../src";
+import {closeTestingConnections, createTestingConnections} from "../../../test/utils/test-utils";
 
 describe("github issues > #736 ClosureEntity should set (composite) primary/unique key in the closure table", () => {
 
     let connections: Connection[];
-    before(async () => {
+    beforeAll(async () => {
         connections = await createTestingConnections({
             entities: [__dirname + "/entity/*{.js,.ts}"],
             schemaCreate: true,
             dropSchema: true,
         });
     });
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("should create composite primary key on closure ancestor and descendant", () => Promise.all(connections.map(async connection => {
+    test("should create composite primary key on closure ancestor and descendant", () => Promise.all(connections.map(async connection => {
         const queryRunner = connection.createQueryRunner();
         const table = await queryRunner.getTable("category_closure");
-        table!.findColumnByName("id_ancestor")!.isPrimary.should.be.true;
-        table!.findColumnByName("id_descendant")!.isPrimary.should.be.true;
+        expect(table!.findColumnByName("id_ancestor")!.isPrimary).toBeTruthy();
+        expect(table!.findColumnByName("id_descendant")!.isPrimary).toBeTruthy();
         await queryRunner.release();
     })));
 
