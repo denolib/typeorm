1a3e55236fce4b89c803ea0ade2e1897d3eab4ee
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-19 12:09:50 +0500

    working "github issues > #423 Cannot use Group as Table name && cannot autoSchemeSync when use alias Entity"

---
+++ tests/github-issues/423/issue-423.spec.ts
@@ -0,0 +1,39 @@
+import "reflect-metadata";
+import {CockroachDriver} from "../../../src/driver/cockroachdb/CockroachDriver";
+import {closeTestingConnections, createTestingConnections} from "../../../test/utils/test-utils";
+import {Connection} from "../../../src";
+
+describe("github issues > #423 Cannot use Group as Table name && cannot autoSchemeSync when use alias Entity", () => {
+
+    let connections: Connection[];
+    beforeAll(async () => connections = await createTestingConnections({
+        entities: [__dirname + "/entity/*{.js,.ts}"],
+        schemaCreate: false,
+        dropSchema: true
+    }));
+    afterAll(() => closeTestingConnections(connections));
+
+    test("should successfully sync schema", () => Promise.all(connections.map(async connection => {
+        await connection.synchronize();
+
+        const queryRunner = connection.createQueryRunner();
+        const table = await queryRunner.getTable("groups");
+        await queryRunner.release();
+
+        table!.should.exist;
+        
+        // CockroachDB stores unique indices as UNIQUE constraints
+        if (connection.driver instanceof CockroachDriver) {
+            expect(table!.uniques.length).toEqual(1);
+            expect(table!.uniques[0].name)!.toEqual("Groups name");
+            expect(table!.uniques[0].columnNames[0]).toEqual("name");
+        } else {
+            expect(table!.indices.length).toEqual(1);
+            expect(table!.indices[0].name)!.toEqual("Groups name");
+            expect(table!.indices[0].columnNames[0]).toEqual("name");
+            expect(table!.indices[0].isUnique)!.toBeTruthy();
+        }
+
+    })));
+
+});
