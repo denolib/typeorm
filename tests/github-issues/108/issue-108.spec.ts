65a37a4e86fcf4da3557be888bdb6b56ed46617b
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-19 12:56:22 +0500

    working "github issues > #108 Error with constraint names on postgres"

---
+++ tests/github-issues/108/issue-108.spec.ts
@@ -0,0 +1,20 @@
+import "reflect-metadata";
+import {closeTestingConnections, createTestingConnections} from "../../../test/utils/test-utils";
+import {Connection} from "../../../src";
+
+describe("github issues > #108 Error with constraint names on postgres", () => {
+
+    let connections: Connection[];
+    beforeAll(async () => connections = await createTestingConnections({
+        entities: [__dirname + "/entity/*{.js,.ts}"],
+        schemaCreate: true,
+        dropSchema: true,
+    }));
+    afterAll(() => closeTestingConnections(connections));
+
+    test("should sync even when there unqiue constraints placed on similarly named columns", () => Promise.all(connections.map(async connection => {
+       // By virtue that we got here means that it must have worked.
+       expect(true).toBeTruthy();
+    })));
+
+});
