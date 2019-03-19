d9513c6a1c591679366d219caa575d7e4b12aba4
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-19 12:04:45 +0500

    working "github issues > #463 saving empty string array"

--- tests/github-issues/463/issue-463.spec.ts
+++ tests/github-issues/463/issue-463.spec.ts
@@ -1,24 +1,24 @@
 import "reflect-metadata";
-import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
-import {Connection} from "../../../src/connection/Connection";
+import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../test/utils/test-utils";
+import {Connection} from "../../../src";
 import {Post} from "./entity/Post";
 
 describe("github issues > #463 saving empty string array", () => {
 
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
+    beforeAll(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
         enabledDrivers: ["postgres"],
     }));
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("should not return array with single empty string if empty array was saved", () => Promise.all(connections.map(async connection => {
+    test("should not return array with single empty string if empty array was saved", () => Promise.all(connections.map(async connection => {
         const post = new Post();
         post.names = [];
         await connection.getRepository(Post).save(post);
         const loadedPost = await connection.getRepository(Post).findOne(1);
-        loadedPost!.names.length.should.be.eql(0);
+        expect(loadedPost!.names.length).toEqual(0);
     })));
 
 });
