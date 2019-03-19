b78c63473b9e83f2b38ec9491cc464ab51b48db5
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-19 12:08:13 +0500

    working "github issues > #433 default value (json) is not getting set in postgreSQL"

--- tests/github-issues/433/issue-433.spec.ts
+++ tests/github-issues/433/issue-433.spec.ts
@@ -1,24 +1,24 @@
 import "reflect-metadata";
-import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
-import {Connection} from "../../../src/connection/Connection";
+import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../test/utils/test-utils";
+import {Connection} from "../../../src";
 import {Post} from "./entity/Post";
 
 describe("github issues > #433 default value (json) is not getting set in postgreSQL", () => {
 
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
+    beforeAll(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
         enabledDrivers: ["postgres"],
     }));
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("should successfully set default value in to JSON type column", () => Promise.all(connections.map(async connection => {
+    test("should successfully set default value in to JSON type column", () => Promise.all(connections.map(async connection => {
         const post = new Post();
         post.id = 1;
         await connection.getRepository(Post).save(post);
         const loadedPost = (await connection.getRepository(Post).findOne(1))!;
-        loadedPost.json.should.be.eql({ hello: "world" });
+        expect(loadedPost.json).toEqual({ hello: "world" });
     })));
 
 });
