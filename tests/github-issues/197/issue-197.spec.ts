26e78ba3d4d923e3c607e915aaa240f69de1f544
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-19 12:35:10 +0500

    working "github issues > #197 Fails to drop indexes when removing fields"

--- tests/github-issues/197/issue-197.spec.ts
+++ tests/github-issues/197/issue-197.spec.ts
@@ -1,20 +1,20 @@
 import "reflect-metadata";
-import { closeTestingConnections, createTestingConnections, reloadTestingDatabases } from "../../utils/test-utils";
-import { Connection } from "../../../src/connection/Connection";
-import { EntityMetadata } from "../../../src/metadata/EntityMetadata";
+import { closeTestingConnections, createTestingConnections, reloadTestingDatabases } from "../../../test/utils/test-utils";
+import { Connection } from "../../../src";
+import { EntityMetadata } from "../../../src";
 import { Person } from "./entity/person";
 
 describe("github issues > #197 Fails to drop indexes when removing fields", () => {
 
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
+    beforeAll(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
         schemaCreate: false,
     }));
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("it should drop the column and the referenced index", () => Promise.all(connections.map(async connection => {
+    test("it should drop the column and the referenced index", () => Promise.all(connections.map(async connection => {
 
         let entityMetadata: EntityMetadata = connection.getMetadata(Person);
         let idx: number = entityMetadata.columns.findIndex(x => x.databaseName === "firstname");
