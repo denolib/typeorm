8db451f6f6bd611cd3fde6784686465c9fc2ea86
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-19 12:03:04 +0500

    working "github issues > #485 If I set the datatype of PrimaryGeneratedColumn to uuid then it is not giving the uuid to the column."

--- tests/github-issues/485/issue-485.spec.ts
+++ tests/github-issues/485/issue-485.spec.ts
@@ -1,22 +1,21 @@
 import "reflect-metadata";
-import {expect} from "chai";
-import {Connection} from "../../../src/connection/Connection";
-import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
+import {Connection} from "../../../src";
+import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../test/utils/test-utils";
 import {Post} from "./entity/Post";
 
 describe("github issues > #485 If I set the datatype of PrimaryGeneratedColumn to uuid then it is not giving the uuid to the column.", () => {
 
     let connections: Connection[];
-    before(async () => {
+    beforeAll(async () => {
         connections = await createTestingConnections({
             entities: [__dirname + "/entity/*{.js,.ts}"],
             enabledDrivers: ["postgres"],
         });
     });
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("should persist uuid correctly when it used as PrimaryGeneratedColumn type", () => Promise.all(connections.map(async connection => {
+    test("should persist uuid correctly when it used as PrimaryGeneratedColumn type", () => Promise.all(connections.map(async connection => {
 
         const postRepository = connection.getRepository(Post);
         const queryRunner = connection.createQueryRunner();
@@ -27,8 +26,8 @@ describe("github issues > #485 If I set the datatype of PrimaryGeneratedColumn t
         const savedPost = await postRepository.save(post);
         const loadedPost = await postRepository.findOne(savedPost.id);
 
-        expect(loadedPost).to.be.not.undefined;
-        expect(loadedPost!.id).to.equal(savedPost.id);
-        table!.findColumnByName("id")!.type.should.be.equal("uuid");
+        expect(loadedPost).not.toBeUndefined();
+        expect(loadedPost!.id).toEqual(savedPost.id);
+        expect(table!.findColumnByName("id")!.type).toEqual("uuid");
     })));
 });
