03a161c78a9a0b40d160c34c5661c63bb758d194
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-19 12:53:20 +0500

    working "github issues > #131 Error with single table inheritance query without additional conditions"

---
+++ tests/github-issues/131/issue-131.spec.ts
@@ -0,0 +1,24 @@
+import "reflect-metadata";
+import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../test/utils/test-utils";
+import {Connection} from "../../../src";
+import {Student} from "./entity/Student";
+import {Employee} from "./entity/Employee";
+
+describe("github issues > #131 Error with single table inheritance query without additional conditions", () => {
+
+    let connections: Connection[];
+    beforeAll(async () => connections = await createTestingConnections({
+        entities: [__dirname + "/entity/*{.js,.ts}"],
+    }));
+    beforeEach(() => reloadTestingDatabases(connections));
+    afterAll(() => closeTestingConnections(connections));
+
+    test("should not fail when querying for single table inheritance model without additional conditions", () => Promise.all(connections.map(async connection => {
+        const employees = await connection.getRepository(Employee).find();
+        expect(employees).not.toBeUndefined();
+
+        const students = await connection.getRepository(Student).find();
+        expect(students).not.toBeUndefined();
+    })));
+
+});
