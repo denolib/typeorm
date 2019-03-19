92da9cbba3ef513f2a2a9de5fdf4cc688b0fe0fc
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-19 12:43:36 +0500

    working "github issues > #175 ManyToMany relation doesn't put an empty array when the relation is empty"

--- tests/github-issues/175/issue-175.spec.ts
+++ tests/github-issues/175/issue-175.spec.ts
@@ -1,20 +1,19 @@
 import "reflect-metadata";
-import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
-import {Connection} from "../../../src/connection/Connection";
+import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../test/utils/test-utils";
+import {Connection} from "../../../src";
 import {Post} from "./entity/Post";
 import {Category} from "./entity/Category";
-import {expect} from "chai";
 
 describe("github issues > #175 ManyToMany relation doesn't put an empty array when the relation is empty", () => {
 
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
+    beforeAll(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
     }));
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
-    it("should return post with categories if they are attached to the post", () => Promise.all(connections.map(async connection => {
+    test("should return post with categories if they are attached to the post", () => Promise.all(connections.map(async connection => {
 
         const category1 = new Category();
         category1.name = "category #1";
@@ -35,8 +34,8 @@ describe("github issues > #175 ManyToMany relation doesn't put an empty array wh
             .where("post.title = :title", { title: "post with categories" })
             .getOne();
 
-        expect(loadedPost).not.to.be.undefined;
-        loadedPost!.should.be.eql({
+        expect(loadedPost).not.toBeUndefined();
+        expect(loadedPost)!.toEqual({
             id: 1,
             title: "post with categories",
             categories: [{
@@ -49,7 +48,7 @@ describe("github issues > #175 ManyToMany relation doesn't put an empty array wh
         });
     })));
     
-    it("should return post with categories even if post with empty categories was saved", () => Promise.all(connections.map(async connection => {
+    test("should return post with categories even if post with empty categories was saved", () => Promise.all(connections.map(async connection => {
 
         const category1 = new Category();
         category1.name = "category #1";
@@ -73,15 +72,15 @@ describe("github issues > #175 ManyToMany relation doesn't put an empty array wh
             .where("post.title = :title", { title: "post without categories" })
             .getOne();
 
-        expect(loadedPost).not.to.be.undefined;
-        loadedPost!.should.be.eql({
+        expect(loadedPost).not.toBeUndefined();
+        expect(loadedPost)!.toEqual({
             id: 1,
             title: "post without categories",
             categories: []
         });
     })));
     
-    it("should return post with categories even if post was saved without categories set", () => Promise.all(connections.map(async connection => {
+    test("should return post with categories even if post was saved without categories set", () => Promise.all(connections.map(async connection => {
 
         const category1 = new Category();
         category1.name = "category #1";
@@ -101,8 +100,8 @@ describe("github issues > #175 ManyToMany relation doesn't put an empty array wh
             .where("post.title = :title", { title: "just post" })
             .getOne();
 
-        expect(loadedPost).not.to.be.undefined;
-        loadedPost!.should.be.eql({
+        expect(loadedPost).not.toBeUndefined();
+        expect(loadedPost)!.toEqual({
             id: 1,
             title: "just post",
             secondaryCategories: []
