e71c078c92fdab875fd4943046a1144cbdce0eba
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-19 12:25:02 +0500

    working "github issues > support of embeddeds that are not set"

---
+++ tests/github-issues/300/issue-300.spec.ts
@@ -0,0 +1,33 @@
+import "reflect-metadata";
+import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../test/utils/test-utils";
+import {Connection} from "../../../src";
+import {Race} from "./entity/Race";
+
+describe("github issues > support of embeddeds that are not set", () => {
+
+    let connections: Connection[];
+    beforeAll(async () => connections = await createTestingConnections({
+        entities: [__dirname + "/entity/*{.js,.ts}"],
+    }));
+    beforeEach(() => reloadTestingDatabases(connections));
+    afterAll(() => closeTestingConnections(connections));
+
+    test("embedded with custom column name should persist and load without errors", () => Promise.all(connections.map(async connection => {
+
+        const race = new Race();
+        race.name = "National Race";
+
+        await connection.manager.save(race);
+
+        const loadedRace = await connection.manager.findOne(Race, { name: "National Race" });
+        expect(loadedRace).toBeDefined();
+        expect(loadedRace!.id).toBeDefined();
+        expect(loadedRace!.name).toEqual("National Race");
+        expect(loadedRace!.duration).toBeDefined();
+        expect(loadedRace!.duration.minutes).toBeNull();
+        expect(loadedRace!.duration.hours).toBeNull();
+        expect(loadedRace!.duration.days).toBeNull();
+
+    })));
+
+});
