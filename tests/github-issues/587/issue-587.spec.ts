8b57e99fbdac2cb9ffeee7c762f84a89458c6eb2
Bakhrom Baubekov <baubekov.bakhrom88@gmail.com>
2019-03-19 11:36:00 +0500

    working "github issues > #587 Ordering of fields in composite indexes defined using Index decorator"

--- tests/github-issues/587/issue-587.spec.ts
+++ tests/github-issues/587/issue-587.spec.ts
@@ -1,26 +1,26 @@
 import "reflect-metadata";
-import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../utils/test-utils";
-import {Connection} from "../../../src/connection/Connection";
+import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../test/utils/test-utils";
+import {Connection} from "../../../src";
 
 describe("github issues > #587 Ordering of fields in composite indexes defined using Index decorator", () => {
 
     let connections: Connection[];
-    before(async () => connections = await createTestingConnections({
+    beforeAll(async () => connections = await createTestingConnections({
         entities: [__dirname + "/entity/*{.js,.ts}"],
         enabledDrivers: ["mysql"]
     }));
     beforeEach(() => reloadTestingDatabases(connections));
-    after(() => closeTestingConnections(connections));
+    afterAll(() => closeTestingConnections(connections));
 
     // this test only works for fields specified as string[]
-    it("should preserve field ordering when fields are specified as string[]", () => Promise.all(connections.map(async connection => {
+    test("should preserve field ordering when fields are specified as string[]", () => Promise.all(connections.map(async connection => {
         connection.entityMetadatas.forEach(entityMetadata => {
             entityMetadata.indices.forEach(index => {
                 if (index.givenColumnNames && index.givenColumnNames instanceof Array) {
                     for (let i = 0; i < index.columns.length; i++) {
                         const givenColumn = (index.givenColumnNames as string[])[i];
                         const actualColumn = index.columns[i];
-                        actualColumn.propertyName.should.equal(givenColumn);
+                        expect(actualColumn.propertyName).toEqual(givenColumn);
                     }
                 }
             });
