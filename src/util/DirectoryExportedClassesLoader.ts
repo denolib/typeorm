d54ffb3a852b435b24a0401a2fea74a5034105ef
uki00a <uki00a@gmail.com>
2020-02-05 23:04:33 +0900

    fix: make `importClassesFromDirectories` work with deno (#8)
    * fix: make `importClassesFromDirectories` work with deno
    * update: implement `PlatformTools.expandGlob`

--- src/util/DirectoryExportedClassesLoader.ts
+++ src/util/DirectoryExportedClassesLoader.ts
@@ -4,40 +4,44 @@ import {Logger} from "../logger/Logger.ts";
 /**
  * Loads all exported classes from the given directory.
  */
-export function importClassesFromDirectories(logger: Logger, directories: string[], formats = [".js", ".ts"]): Function[] {
+export async function importClassesFromDirectories(logger: Logger, directories: string[], formats = [".js", ".ts"]): Promise<Function[]> {
 
     const logLevel = "info";
     const classesNotFoundMessage = "No classes were found using the provided glob pattern: ";
     const classesFoundMessage = "All classes found using provided glob pattern";
-    function loadFileClasses(exported: any, allLoaded: Function[]) {
-        if (typeof exported === "function" || exported instanceof EntitySchema) {
-            allLoaded.push(exported);
+    function loadFileClasses(mod: any, allLoaded: Function[]) {
+        if (typeof mod === "function" || mod instanceof EntitySchema) {
+            allLoaded.push(mod);
 
-        } else if (Array.isArray(exported)) {
-            exported.forEach((i: any) => loadFileClasses(i, allLoaded));
+        } else if (Array.isArray(mod)) {
+            mod.forEach((i: any) => loadFileClasses(i, allLoaded));
 
-        } else if (typeof exported === "object" && exported !== null) {
-            Object.keys(exported).forEach(key => loadFileClasses(exported[key], allLoaded));
+        } else {
+            Object.keys(mod).forEach(key => loadFileClasses(mod[key], allLoaded));
 
         }
         return allLoaded;
     }
 
-    const allFiles = directories.reduce((allDirs, dir) => {
-        return allDirs.concat(PlatformTools.load("glob").sync(PlatformTools.pathNormalize(dir)));
-    }, [] as string[]);
+    const allFiles = [] as string[];
+    for (const dir of directories) {
+        for await (const {filename} of PlatformTools.expandGlob(dir)) {
+            allFiles.push(filename);
+        }
+    }
 
     if (directories.length > 0 && allFiles.length === 0) {
         logger.log(logLevel, `${classesNotFoundMessage} "${directories}"`);
     } else if (allFiles.length > 0) {
         logger.log(logLevel, `${classesFoundMessage} "${directories}" : "${allFiles}"`);
     }
-    const dirs = allFiles
+    const promises = allFiles
         .filter(file => {
             const dtsExtension = file.substring(file.length - 5, file.length);
             return formats.indexOf(PlatformTools.pathExtname(file)) !== -1 && dtsExtension !== ".d.ts";
         })
-        .map(file => PlatformTools.load(PlatformTools.pathResolve(file)));
+        .map(file => import(PlatformTools.pathResolve(file)));
+    const dirs = (await Promise.all(promises)).map(mod => mod.default ? mod.default : mod);
 
     return loadFileClasses(dirs, []);
 }
