099afc84f360460874f89dc46b3029b7276acf77
uki00a <uki00a@gmail.com>
2020-02-08 16:49:01 +0900

    fix: test/functional/columns/no-select

--- test.ts
+++ test.ts
@@ -3,5 +3,6 @@ import "./test/integration/sample1-simple-entity.ts";
 import "./test/functional/cascades/cascade-insert-from-both-sides/cascade-insert-from-both-sides.ts";
 import "./test/functional/columns/embedded-columns/columns-embedded-columns.ts";
 import "./test/functional/columns/getters-setters/columns-getters-setters.ts";
+import "./test/functional/columns/no-select/columns-no-select.ts";
 
 runTests();
