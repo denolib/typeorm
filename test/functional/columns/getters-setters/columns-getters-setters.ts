3e6cda25f4532170703c19f11b9ab711426f34ec
uki00a <uki00a@gmail.com>
2020-02-08 16:42:14 +0900

    fix: test/functional/columns/getters-setters

--- test/functional/columns/getters-setters/columns-getters-setters.ts
+++ test/functional/columns/getters-setters/columns-getters-setters.ts
@@ -1,8 +1,8 @@
-import "reflect-metadata";
-import {expect} from "chai";
-import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../utils/test-utils";
-import {Connection} from "../../../../src/connection/Connection";
-import {Post} from "./entity/Post";
+import {runIfMain} from "../../../deps/mocha.ts";
+import {expect} from "../../../deps/chai.ts";
+import {closeTestingConnections, createTestingConnections, reloadTestingDatabases} from "../../../utils/test-utils.ts";
+import {Connection} from "../../../../src/connection/Connection.ts";
+import {Post} from "./entity/Post.ts";
 
 describe("columns > getters and setters", () => {
 
@@ -34,3 +34,5 @@ describe("columns > getters and setters", () => {
 
 
 });
+
+runIfMain(import.meta);
