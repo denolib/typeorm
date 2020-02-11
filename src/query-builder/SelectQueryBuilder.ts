denolib/typeorm

commit 5cbe0f64ffe4568281f952da3258fcdde5b4a33f

fix: make compilation pass with deno (#2)
* chore: initial setup
* fix: make compilation pass with deno

src/query-builder/SelectQueryBuilder.ts
@@ -1,41 +1,34 @@
-import {SapDriver} from "../driver/sap/SapDriver";
-import {RawSqlResultsToEntityTransformer} from "./transformer/RawSqlResultsToEntityTransformer";
-import {ObjectLiteral} from "../common/ObjectLiteral";
-import {SqlServerDriver} from "../driver/sqlserver/SqlServerDriver";
-import {PessimisticLockTransactionRequiredError} from "../error/PessimisticLockTransactionRequiredError";
-import {NoVersionOrUpdateDateColumnError} from "../error/NoVersionOrUpdateDateColumnError";
-import {OptimisticLockVersionMismatchError} from "../error/OptimisticLockVersionMismatchError";
-import {OptimisticLockCanNotBeUsedError} from "../error/OptimisticLockCanNotBeUsedError";
-import {JoinAttribute} from "./JoinAttribute";
-import {RelationIdAttribute} from "./relation-id/RelationIdAttribute";
-import {RelationCountAttribute} from "./relation-count/RelationCountAttribute";
-import {RelationIdLoader} from "./relation-id/RelationIdLoader";
-import {RelationIdMetadataToAttributeTransformer} from "./relation-id/RelationIdMetadataToAttributeTransformer";
-import {RelationCountLoader} from "./relation-count/RelationCountLoader";
-import {RelationCountMetadataToAttributeTransformer} from "./relation-count/RelationCountMetadataToAttributeTransformer";
-import {QueryBuilder} from "./QueryBuilder";
-import {ReadStream} from "../platform/PlatformTools";
-import {LockNotSupportedOnGivenDriverError} from "../error/LockNotSupportedOnGivenDriverError";
-import {MysqlDriver} from "../driver/mysql/MysqlDriver";
-import {PostgresDriver} from "../driver/postgres/PostgresDriver";
-import {OracleDriver} from "../driver/oracle/OracleDriver";
-import {SelectQuery} from "./SelectQuery";
-import {EntityMetadata} from "../metadata/EntityMetadata";
-import {ColumnMetadata} from "../metadata/ColumnMetadata";
-import {OrderByCondition} from "../find-options/OrderByCondition";
-import {QueryExpressionMap} from "./QueryExpressionMap";
-import {ObjectType} from "../common/ObjectType";
-import {QueryRunner} from "../query-runner/QueryRunner";
-import {WhereExpression} from "./WhereExpression";
-import {Brackets} from "./Brackets";
-import {AbstractSqliteDriver} from "../driver/sqlite-abstract/AbstractSqliteDriver";
-import {QueryResultCacheOptions} from "../cache/QueryResultCacheOptions";
-import {OffsetWithoutLimitNotSupportedError} from "../error/OffsetWithoutLimitNotSupportedError";
-import {BroadcasterResult} from "../subscriber/BroadcasterResult";
-import {SelectQueryBuilderOption} from "./SelectQueryBuilderOption";
-import {ObjectUtils} from "../util/ObjectUtils";
-import {DriverUtils} from "../driver/DriverUtils";
-import {AuroraDataApiDriver} from "../driver/aurora-data-api/AuroraDataApiDriver";
+import {RawSqlResultsToEntityTransformer} from "./transformer/RawSqlResultsToEntityTransformer.ts";
+import {ObjectLiteral} from "../common/ObjectLiteral.ts";
+import {PessimisticLockTransactionRequiredError} from "../error/PessimisticLockTransactionRequiredError.ts";
+import {NoVersionOrUpdateDateColumnError} from "../error/NoVersionOrUpdateDateColumnError.ts";
+import {OptimisticLockVersionMismatchError} from "../error/OptimisticLockVersionMismatchError.ts";
+import {OptimisticLockCanNotBeUsedError} from "../error/OptimisticLockCanNotBeUsedError.ts";
+import {JoinAttribute} from "./JoinAttribute.ts";
+import {RelationIdAttribute} from "./relation-id/RelationIdAttribute.ts";
+import {RelationCountAttribute} from "./relation-count/RelationCountAttribute.ts";
+import {RelationIdLoader} from "./relation-id/RelationIdLoader.ts";
+import {RelationIdMetadataToAttributeTransformer} from "./relation-id/RelationIdMetadataToAttributeTransformer.ts";
+import {RelationCountLoader} from "./relation-count/RelationCountLoader.ts";
+import {RelationCountMetadataToAttributeTransformer} from "./relation-count/RelationCountMetadataToAttributeTransformer.ts";
+import {QueryBuilder} from "./QueryBuilder.ts";
+import {ReadStream} from "../platform/PlatformTools.ts";
+import {LockNotSupportedOnGivenDriverError} from "../error/LockNotSupportedOnGivenDriverError.ts";
+import {SelectQuery} from "./SelectQuery.ts";
+import {EntityMetadata} from "../metadata/EntityMetadata.ts";
+import {ColumnMetadata} from "../metadata/ColumnMetadata.ts";
+import {OrderByCondition} from "../find-options/OrderByCondition.ts";
+import {QueryExpressionMap} from "./QueryExpressionMap.ts";
+import {ObjectType} from "../common/ObjectType.ts";
+import {QueryRunner} from "../query-runner/QueryRunner.ts";
+import {WhereExpression} from "./WhereExpression.ts";
+import {Brackets} from "./Brackets.ts";
+import {AbstractSqliteDriver} from "../driver/sqlite-abstract/AbstractSqliteDriver.ts";
+import {QueryResultCacheOptions} from "../cache/QueryResultCacheOptions.ts";
+import {BroadcasterResult} from "../subscriber/BroadcasterResult.ts";
+import {SelectQueryBuilderOption} from "./SelectQueryBuilderOption.ts";
+import {ObjectUtils} from "../util/ObjectUtils.ts";
+import {DriverUtils} from "../driver/DriverUtils.ts";
 
 /**
  * Allows to build complex sql queries in a fashion way and execute those queries.
@@ -1394,21 +1387,6 @@ export class SelectQueryBuilder<Entity> extends QueryBuilder<Entity> implements 
         if (allSelects.length === 0)
             allSelects.push({ selection: "*" });
 
-        let lock: string = "";
-        if (this.connection.driver instanceof SqlServerDriver) {
-            switch (this.expressionMap.lockMode) {
-                case "pessimistic_read":
-                    lock = " WITH (HOLDLOCK, ROWLOCK)";
-                    break;
-                case "pessimistic_write":
-                    lock = " WITH (UPDLOCK, ROWLOCK)";
-                    break;
-                case "dirty_read":
-                    lock = " WITH (NOLOCK)";
-                    break;
-            }
-        }
-
         // create a selection query
         const froms = this.expressionMap.aliases
             .filter(alias => alias.type === "from" && (alias.tablePath || alias.subQuery))
@@ -1422,26 +1400,14 @@ export class SelectQueryBuilder<Entity> extends QueryBuilder<Entity> implements 
         const select = this.createSelectDistinctExpression();
         const selection = allSelects.map(select => select.selection + (select.aliasName ? " AS " + this.escape(select.aliasName) : "")).join(", ");
 
-        return select + selection + " FROM " + froms.join(", ") + lock;
+        return select + selection + " FROM " + froms.join(", ");
     }
 
     /**
      * Creates select | select distinct part of SQL query.
      */
     protected createSelectDistinctExpression(): string {
-        const {selectDistinct, selectDistinctOn} = this.expressionMap;
-        const {driver} = this.connection;
-
-        let select = "SELECT ";
-        if (driver instanceof PostgresDriver && selectDistinctOn.length > 0) {
-            const selectDistinctOnMap = selectDistinctOn.map(
-              (on) => this.replacePropertyNames(on)
-            ).join(", ");
-
-            select = `SELECT DISTINCT ON (${selectDistinctOnMap}) `;
-        } else if (selectDistinct) {
-            select = "SELECT DISTINCT ";
-        }
+        let select = "SELECT DISTINCT ";
 
         return select;
     }
@@ -1575,34 +1541,7 @@ export class SelectQueryBuilder<Entity> extends QueryBuilder<Entity> implements 
             limit = this.expressionMap.take;
         }
 
-        if (this.connection.driver instanceof SqlServerDriver) {
-            // Due to a limitation in SQL Server's parser implementation it does not support using
-            // OFFSET or FETCH NEXT without an ORDER BY clause being provided. In cases where the
-            // user does not request one we insert a dummy ORDER BY that does nothing and should
-            // have no effect on the query planner or on the order of the results returned.
-            // https://dba.stackexchange.com/a/193799
-            let prefix = "";
-            if ((limit || offset) && Object.keys(this.expressionMap.allOrderBys).length <= 0) {
-                prefix = " ORDER BY (SELECT NULL)";
-            }
-
-            if (limit && offset)
-                return prefix + " OFFSET " + offset + " ROWS FETCH NEXT " + limit + " ROWS ONLY";
-            if (limit)
-                return prefix + " OFFSET 0 ROWS FETCH NEXT " + limit + " ROWS ONLY";
-            if (offset)
-                return prefix + " OFFSET " + offset + " ROWS";
-
-        } else if (this.connection.driver instanceof MysqlDriver || this.connection.driver instanceof AuroraDataApiDriver || this.connection.driver instanceof SapDriver) {
-
-            if (limit && offset)
-                return " LIMIT " + limit + " OFFSET " + offset;
-            if (limit)
-                return " LIMIT " + limit;
-            if (offset)
-                throw new OffsetWithoutLimitNotSupportedError();
-
-        } else if (this.connection.driver instanceof AbstractSqliteDriver) {
+        if (this.connection.driver instanceof AbstractSqliteDriver) {
 
             if (limit && offset)
                 return " LIMIT " + limit + " OFFSET " + offset;
@@ -1610,15 +1549,6 @@ export class SelectQueryBuilder<Entity> extends QueryBuilder<Entity> implements 
                 return " LIMIT " + limit;
             if (offset)
                 return " LIMIT -1 OFFSET " + offset;
-
-        } else if (this.connection.driver instanceof OracleDriver) {
-
-            if (limit && offset)
-                return " OFFSET " + offset + " ROWS FETCH NEXT " + limit + " ROWS ONLY";
-            if (limit)
-                return " FETCH NEXT " + limit + " ROWS ONLY";
-            if (offset)
-                return " OFFSET " + offset + " ROWS";
 
         } else {
             if (limit && offset)
@@ -1636,34 +1566,11 @@ export class SelectQueryBuilder<Entity> extends QueryBuilder<Entity> implements 
      * Creates "LOCK" part of SQL query.
      */
     protected createLockExpression(): string {
-        const driver = this.connection.driver;
         switch (this.expressionMap.lockMode) {
             case "pessimistic_read":
-                if (driver instanceof MysqlDriver || driver instanceof AuroraDataApiDriver) {
-                    return " LOCK IN SHARE MODE";
-
-                } else if (driver instanceof PostgresDriver) {
-                    return " FOR SHARE";
-
-                } else if (driver instanceof OracleDriver) {
-                    return " FOR UPDATE";
-
-                } else if (driver instanceof SqlServerDriver) {
-                    return "";
-
-                } else {
-                    throw new LockNotSupportedOnGivenDriverError();
-                }
+                throw new LockNotSupportedOnGivenDriverError();
             case "pessimistic_write":
-                if (driver instanceof MysqlDriver || driver instanceof AuroraDataApiDriver || driver instanceof PostgresDriver || driver instanceof OracleDriver) {
-                    return " FOR UPDATE";
-
-                } else if (driver instanceof SqlServerDriver) {
-                    return "";
-
-                } else {
-                    throw new LockNotSupportedOnGivenDriverError();
-                }
+                throw new LockNotSupportedOnGivenDriverError();
             default:
                 return "";
         }
@@ -1712,20 +1619,6 @@ export class SelectQueryBuilder<Entity> extends QueryBuilder<Entity> implements 
         return allColumns.map(column => {
             const selection = this.expressionMap.selects.find(select => select.selection === aliasName + "." + column.propertyPath);
             let selectionPath = this.escape(aliasName) + "." + this.escape(column.databaseName);
-            if (this.connection.driver.spatialTypes.indexOf(column.type) !== -1) {
-                if (this.connection.driver instanceof MysqlDriver || this.connection.driver instanceof AuroraDataApiDriver) {
-                    const useLegacy = this.connection.driver.options.legacySpatialSupport;
-                    const asText = useLegacy ? "AsText" : "ST_AsText";
-                    selectionPath = `${asText}(${selectionPath})`;
-                }
-
-                if (this.connection.driver instanceof PostgresDriver)
-                    // cast to JSON to trigger parsing in the driver
-                    selectionPath = `ST_AsGeoJSON(${selectionPath})::json`;
-
-                if (this.connection.driver instanceof SqlServerDriver)
-                    selectionPath = `${selectionPath}.ToString()`;
-            }
             return {
                 selection: selectionPath,
                 aliasName: selection && selection.aliasName ? selection.aliasName : DriverUtils.buildColumnAlias(this.connection.driver, aliasName, column.databaseName),

commit 1bd9e9ba466a5b137621999d6ee3b9499a2bc4a3

fix: the bug of `SelectQueryBuilder#createSelectDistinctExpression` caused by 5cbe0f64ffe4568281f952da3258fcdde5b4a33f (#10)
See:

- https://github.com/uki00a/typeorm-deno-fork/pull/2/files#diff-7da0fc32ecaf4de82fdafaaac5d5c9aeL1432

src/query-builder/SelectQueryBuilder.ts
@@ -1407,7 +1407,20 @@ export class SelectQueryBuilder<Entity> extends QueryBuilder<Entity> implements 
      * Creates select | select distinct part of SQL query.
      */
     protected createSelectDistinctExpression(): string {
-        let select = "SELECT DISTINCT ";
+        const {selectDistinct, selectDistinctOn} = this.expressionMap;
+        //const {driver} = this.connection;
+
+        let select = "SELECT ";
+        // TODO(uki00a) uncomment this when PostgresDriver is implemented.
+        /*if (driver instanceof PostgresDriver && selectDistinctOn.length > 0) {
+            const selectDistinctOnMap = selectDistinctOn.map(
+              (on) => this.replacePropertyNames(on)
+            ).join(", ");
+
+            select = `SELECT DISTINCT ON (${selectDistinctOnMap}) `;
+        } */if (selectDistinct) {
+            select = "SELECT DISTINCT ";
+        }
 
         return select;
     }
@@ -1730,7 +1743,7 @@ export class SelectQueryBuilder<Entity> extends QueryBuilder<Entity> implements 
             });
 
             rawResults = await new SelectQueryBuilder(this.queryBuilderFactory, this.connection, queryRunner)
-                .select(`DISTINCT ${querySelects.join(", ")}`)
+                .select(`${querySelects.join(", ")}`)
                 .addSelect(selects)
                 .from(`(${this.clone().orderBy().getQuery()})`, "distinctAlias")
                 .offset(this.expressionMap.skip)