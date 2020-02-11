6d76f33c0acecbab8c0fc8a5e2266482fb9ae5ad
uki00a <uki00a@gmail.com>
2020-02-11 19:22:37 +0900

    fix: test/functional/persistence/bulk-insert-remove-optimization

--- test.ts
+++ test.ts
@@ -99,5 +99,6 @@ import "./test/functional/multi-schema-and-database/custom-junction-database/cus
 import "./test/functional/multi-schema-and-database/custom-junction-schema/custom-junction-schema.ts";
 import "./test/functional/multi-schema-and-database/multi-schema-and-database-basic-functionality/multi-schema-and-database-basic-functionality.ts";
 import "./test/functional/persistence/basic-functionality/persistence-basic-functionality.ts";
+import "./test/functional/persistence/bulk-insert-remove-optimization/bulk-insert-remove-optimization.ts";
 
 runTests();
