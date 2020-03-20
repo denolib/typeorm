608edfc2515f7d1b984b6a2e44ececa0df5962b6
uki00a <uki00a@gmail.com>
2020-03-20 15:50:06 +0900

    fix: test/functional/transaction/transactional-decorator

--- test/functional/transaction/transaction-decorator/controller/PostController.ts
+++ test/functional/transaction/transaction-decorator/controller/PostController.ts
@@ -11,9 +11,9 @@ import {CategoryRepository} from "../repository/CategoryRepository.ts";
 export class PostController {
 
     @Transaction("mysql") // "mysql" is a connection name. you can not pass it if you are using default connection.
-    async save(post: Post, category: Category, @TransactionManager() entityManager: EntityManager) {
-        await entityManager.save(post);
-        await entityManager.save(category);
+    async save(post: Post, category: Category, @TransactionManager() entityManager?: EntityManager) {
+        await entityManager!.save(post);
+        await entityManager!.save(category);
     }
 
     // this save is not wrapped into the transaction
@@ -27,19 +27,19 @@ export class PostController {
     async saveWithRepository(
         post: Post,
         category: Category,
-        /*@TransactionRepository(Post)*/postRepository: Repository<Post>,
-        /*@TransactionRepository()*/categoryRepository: CategoryRepository,
+        /*@TransactionRepository(Post)*/postRepository?: Repository<Post>,
+        /*@TransactionRepository()*/categoryRepository?: CategoryRepository,
     ) {
-        await postRepository.save(post);
-        await categoryRepository.save(category);
+        await postRepository!.save(post);
+        await categoryRepository!.save(category);
 
-        return categoryRepository.findByName(category.name);
+        return categoryRepository!.findByName(category.name);
     }
 
     @Transaction({ connectionName: "mysql", isolation: "SERIALIZABLE" }) // "mysql" is a connection name. you can not pass it if you are using default connection.
-    async saveWithNonDefaultIsolation(post: Post, category: Category, @TransactionManager() entityManager: EntityManager) {
-        await entityManager.save(post);
-        await entityManager.save(category);
+    async saveWithNonDefaultIsolation(post: Post, category: Category, @TransactionManager() entityManager?: EntityManager) {
+        await entityManager!.save(post);
+        await entityManager!.save(category);
     }
 
 }
