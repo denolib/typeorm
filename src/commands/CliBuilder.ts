bd6528593b66ba7f4825e7ba9bf92d4e5c14a2cc
uki00a <uki00a@gmail.com>
2020-03-26 23:38:50 +0900

    fix: add tests

--- src/commands/CliBuilder.ts
+++ src/commands/CliBuilder.ts
@@ -1,19 +1,23 @@
 import { cac } from "../../vendor/https/unpkg.com/cac/mod.js";
 
+/**
+ * This is a wrapper of cac.js that provides yargs-like API.
+ */
 interface CliBuilder {
-  command(command: CommandModule): CliBuilder;
-  parse(): object;
+    command(command: CommandModule): CliBuilder;
+    parse(args?: string[]): object;
 }
 
 export interface CommandBuilder {
-  option(
-      shortname: string,
-      options: {
-          alias?: string
-          default?: any
-          describe: string
-      }
-  ): CommandBuilder;
+    option(
+        shortname: string,
+        options: {
+            alias?: string
+            default?: any
+            demand?: boolean,
+            describe: string
+        }
+    ): CommandBuilder;
 }
 
 export interface CommandModule {
@@ -31,15 +35,31 @@ export interface Args {
 function createCommandBuilder(cli: ReturnType<typeof cac>, module: CommandModule): CommandBuilder {
     const command = cli
         .command(module.command + " [...args]", module.describe, {})
-        .action((args: any[], options: object) => {
-            return module.handler({
-                _: [module.command, ...args],
+        .action((_args: any[], options: object) => {
+            const args = {
+                _: [module.command, ..._args],
                 ...options
-            });
+            } as Args;
+            validateRequiredOptions(args);
+            return module.handler(args);
         });
+
+    const requiredOptions = [] as string[];
+    function validateRequiredOptions(args: Args): void {
+        for (const requiredOption of requiredOptions) {
+            if (args[requiredOption] == null) {
+                throw new Error(`missing '-${requiredOption}' option for '${module.command}' command`);
+            }
+        }
+    }
+
     const builder = {
         option(shortname, options) {
-            const option = options.alias ? `-${shortname}, --${options.alias}` : `-${shortname}`;
+            if (options.demand) {
+                requiredOptions.push(shortname);
+            }
+            const arg = options.demand ? '<arg>' : '[arg]';
+            const option = options.alias ? `-${shortname}, --${options.alias} ${arg}` : `-${shortname} ${arg}`;
             command.option(option, options.describe, {
                 default: options.default
             });
@@ -57,8 +77,8 @@ export function createCliBuilder(): CliBuilder {
         module.builder(createCommandBuilder(cli, module));
         return builder;
     },
-    parse() {
-        return cli.parse();
+    parse(args: string[]) {
+        return arguments.length === 0 ? cli.parse() : cli.parse(args);
     }
   } as CliBuilder;
   return builder;
