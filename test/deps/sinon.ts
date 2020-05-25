19e34065788399b7422619c7cdb038d8af2ffcda
uki00a <uki00a@gmail.com>
2020-05-26 04:54:22 +0900

    fix

--- test/deps/sinon.ts
+++ test/deps/sinon.ts
@@ -1,4 +1,4 @@
-// @ts-ignore
-import sinon from 'https://cdn.pika.dev/sinon@^8.1.1';
+import sinon from "https://dev.jspm.io/sinon@8.1.1";
+//import sinon from 'https://cdn.pika.dev/sinon@^8.1.1';
 
 export default sinon;
