ff2f21df0ab9c5d55ae269222ab200d794258ed2
uki00a <uki00a@gmail.com>
2020-05-30 23:22:09 +0900

    update: bump deno to v1.0.3 and std to v0.54.0

--- vendor/https/deno.land/std/fmt/colors.ts
+++ vendor/https/deno.land/std/fmt/colors.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@0.53.0/fmt/colors.ts";
+export * from "https://deno.land/std@0.54.0/fmt/colors.ts";
--- vendor/https/deno.land/std/path/mod.ts
+++ vendor/https/deno.land/std/path/mod.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@0.53.0/path/mod.ts";
+export * from "https://deno.land/std@0.54.0/path/mod.ts";
