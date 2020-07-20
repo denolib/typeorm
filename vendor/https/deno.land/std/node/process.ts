2dd22743335bd4f42a26f567de98010b6b7c1ed8
uki00a <uki00a@gmail.com>
2020-07-21 08:53:04 +0900

    fix: support deno@v1.2.0 (#85)
    * update: bump deno to v1.2.0 and std to v0.61.0
    * update: use deno-postgres@28249fb

--- vendor/https/deno.land/std/node/process.ts
+++ vendor/https/deno.land/std/node/process.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/std@0.59.0/node/process.ts";
-export { default } from "https://deno.land/std@0.59.0/node/process.ts";
+export * from "https://deno.land/std@v0.61.0/node/process.ts";
+export { default } from "https://deno.land/std@v0.61.0/node/process.ts";
--- vendor/https/deno.land/x/postgres/mod.ts
+++ vendor/https/deno.land/x/postgres/mod.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/x/postgres@v0.4.1/mod.ts";
+export * from "https://deno.land/x/postgres@28249fbec10f7f1581f405a5a5b4b105a838629b/mod.ts";
