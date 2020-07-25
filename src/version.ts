a834d6719ae965829994ee5bb0b08021fb86b217
uki00a <uki00a@gmail.com>
2020-07-25 11:52:08 +0900

    release: v0.2.23-rc6

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
-export const VERSION = "v0.2.23-rc5";
+export const VERSION = "v0.2.23-rc6";
 export const MOD_URL = `https://denolib.com/denolib/typeorm@${VERSION}/mod.ts`;
 export const CLI_URL = `https://denolib.com/denolib/typeorm@${VERSION}/cli.ts`;
