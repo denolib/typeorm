8e797addab8dfd717406f04658695758b18bdf55
uki00a <uki00a@gmail.com>
2020-08-10 08:56:43 +0900

    update: bump deno to v1.2.3 and std to v0.64.0

--- vendor/https/deno.land/std/async/deferred.ts
+++ vendor/https/deno.land/std/async/deferred.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@v0.63.0/async/deferred.ts";
+export * from "https://deno.land/std@v0.64.0/async/deferred.ts";
--- vendor/https/deno.land/std/fs/empty_dir.ts
+++ vendor/https/deno.land/std/fs/empty_dir.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@v0.63.0/fs/empty_dir.ts";
+export * from "https://deno.land/std@v0.64.0/fs/empty_dir.ts";
