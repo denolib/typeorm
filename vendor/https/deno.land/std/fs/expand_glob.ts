1ae075490e100337acc0d97127c0a32f8cc4f053
uki00a <uki00a@gmail.com>
2020-08-10 09:03:17 +0900

    update: bump deno to v1.2.3 and std to v0.64.0 (#112)

--- vendor/https/deno.land/std/fs/expand_glob.ts
+++ vendor/https/deno.land/std/fs/expand_glob.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@v0.63.0/fs/expand_glob.ts";
+export * from "https://deno.land/std@v0.64.0/fs/expand_glob.ts";
--- vendor/https/deno.land/std/hash/sha256.ts
+++ vendor/https/deno.land/std/hash/sha256.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@v0.63.0/hash/sha256.ts";
+export * from "https://deno.land/std@v0.64.0/hash/sha256.ts";
