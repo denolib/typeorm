fbf325216679aa830f2fca260d7c77c9c2905d8a
uki00a <uki00a@gmail.com>
2020-09-19 17:00:08 +0900

    feat(cli): print base typeorm version

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,4 @@
 export const VERSION = "v0.2.23-rc8";
+export const BASE_TYPEORM_VERSION = "v0.2.22";
 export const MOD_URL = `https://denolib.com/denolib/typeorm@${VERSION}/mod.ts`;
 export const CLI_URL = `https://denolib.com/denolib/typeorm@${VERSION}/cli.ts`;
